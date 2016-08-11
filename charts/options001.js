38d6440902d82e1c48ebec4e7fe1543104336206
jb3-2 <jonas.bieri@gmail.com>
2016-08-11 15:36:07 +0200

    Change export menu texts

--- charts/options001.js
+++ charts/options001.js
@@ -1,14 +1,32 @@
 Highcharts.setOptions({
 	"lang": {
-		"downloadJPEG": "JPEG erstellen",
+		"downloadJPEG": "JPEG",
 		"resetZoom": "Zoom zurücksetzen",
 		"resetZoomTitle": "Zoom zurücksetzen 1:1",
 		"printChart": "Drucken",
-		"downloadSVG": "SVG erstellen",
-		"downloadPNG": "PNG erstellen",
+		"downloadSVG": "SVG",
+		"downloadPNG": "PNG",
 		"decimalPoint": ",",
 		"contextButtonTitle": "Exportieren und drucken",
-		"downloadPDF": "PDF erstellen",
+		"downloadPDF": "PDF",
+		"downloadCSV": 'CSV',
+		"downloadXLS": 'XLS',		
     "thousandsSep": " "  
-	}
+	},
+	"exporting": {
+        "sourceWidth": null,
+        "scale": 5,
+        buttons: {
+            contextButton: {
+                "text": "",
+                menuItems: Highcharts.getOptions().exporting.buttons.contextButton.menuItems.slice(0, 8)                
+            }
+        }
+    },
+    navigation: {
+        menuItemStyle: {
+            fontFamily: Highcharts.SVGRenderer.prototype.getStyle().fontFamily,
+            padding: '0px 10px'
+        }
+    }
 });
