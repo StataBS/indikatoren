af998d6a6faa632b44f516c14ee9cef75b5bfa58
jb3-2 <jonas.bieri@gmail.com>
2016-08-11 12:51:52 +0200

    Change some chart context menu texts

--- charts/options001.js
+++ charts/options001.js
@@ -1,7 +1,14 @@
 Highcharts.setOptions({
-  "lang": {
-    "decimalPoint": ",",
-    "resetZoom": "Zoom zurücksetzen",
+	"lang": {
+		"downloadJPEG": "JPEG erstellen",
+		"resetZoom": "Zoom zurücksetzen",
+		"resetZoomTitle": "Zoom zurücksetzen 1:1",
+		"printChart": "Drucken",
+		"downloadSVG": "SVG erstellen",
+		"downloadPNG": "PNG erstellen",
+		"decimalPoint": ",",
+		"contextButtonTitle": "Exportieren und drucken",
+		"downloadPDF": "PDF erstellen",
     "thousandsSep": " "  
-  }
+	}
 });
