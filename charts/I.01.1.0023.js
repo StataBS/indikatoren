04274ce1883cba8a5a9ae044a2663a5e7ade4ace
lib <bjoern.lietzke@bs.ch>
2016-08-11 10:43:03 +0200

    Update I.01.1.0023.js

--- charts/I.01.1.0023.js
+++ charts/I.01.1.0023.js
@@ -1,80 +1,14 @@
 chartOptions['I.01.1.0023'] = {
-  "plotOptions": {
-    "series": {
-      "dataLabels": {
-        "enabled": true,
-          //display label at first or last point: https://gist.github.com/jeremywrowe/3506869
-          formatter: function() {
-            var last  = this.series.data[this.series.data.length - 1];
-            var first  = this.series.data[0];
-            if (this.point.y === first.y || this.point.y === last.y ) {
-              return Highcharts.numberFormat(this.point.y, 0, ",", " ");
-            }
-            return "";
-          }
-      }
-    }
+
+  "chart": {
+    "renderTo": "container-I.01.1.0023"
   },
-  "yAxis": {
-    "title": {
-      "style": {
-        "color": "#000000",
-        "fontSize": null
-      },
-      "text": null
-    },
-    "labels": {
-      "format": "{value:,.0f}",
-      "style": {
-        "color": "#000000"
-      }
-    },
-    "min": 0
+  "title": {
+    "text": "Indikator 1.1.a: Bevölkerung am Jahresende",
   },
-  "xAxis": {
-    "title": {
-      "style": {
-        "color": "#000000"
-      }
-    },
-    "labels": {
-      "style": {
-        "color": "#000000"
-      }
-    },
-    "tickColor": "#FFFFFF",
-    "tickInterval": 1
-  },
-  "exporting": {
-	    "sourceWidth": null,
-	    "scale": 5,
-	    buttons: {
-	      contextButton: {
-	        menuItems: Highcharts.getOptions().exporting.buttons.contextButton.menuItems.slice(0, 8)
-	      }
-	    }
-	  },
   "credits": {
     "text": "Quelle: Kantonale Bevölkerungsstatisstik;<br/>Statistisches Amt des Kantons Basel-Stadt",
-    "href": "http://www.statistik.bs.ch",
-    "enabled": true,
-    "style": {
-      "color": "#000000",
-      "fontSize": 10
-    },
-    "position": {
-      "align": "left",
-      "verticalAlign": "bottom",
-      x: 10,
-      y: -17
-    }
-  },
-  "legend": {
-    "enabled": false,
-    "layout": "vertical",
-    "verticalAlign": "middle",
-    "align": "right"
-  },
+  },  
   "series": [
     {
       "marker": {
@@ -116,40 +50,6 @@ chartOptions['I.01.1.0023'] = {
       "index": 0,
       "color": "#008AC3"
     }
-  ],
-  "tooltip": {
-    "shared": false
-  },
-  "title": {
-    "style": {
-      "fontSize": 14,
-      "fontWeight": "bold",
-      "color": "#000000"
-    },
-    "text": "Indikator 1.1.a: Bevölkerung am Jahresende",
-    "align": "left"
-  },
-  "subtitle": {
-    "style": {
-      "fontSize": 12,
-      "fontWeight": "normal",
-      "fontFamily": "Arial",
-      "color": "#000000"
-    },
-    "text": "",
-    "align": "left"
-  },
-  "chart": {
-    "renderTo": "container-I.01.1.0023",
-    "borderColor": "#fbfbfb",
-    "backgroundColor": "#fbfbfb",
-    "zoomType": "xy",
-    "width": 485,
-    "marginBottom": 65,
-    "marginTop": 75,
-    "style": {
-      "fontFamily": "Arial"
-    },
-    "type": "line"    
-  }
+  ]
+
 };
