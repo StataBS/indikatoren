f909437909db8376d29f9b28f226bd16a1cdd878
jb3-2 <jonas.bieri@gmail.com>
2016-08-11 13:35:56 +0200

    Add sozialhilfequote chart

---
+++ charts/dotplot.js
@@ -0,0 +1,122 @@
+var template = {
+    "chart": {		
+        events:{
+            load: function() {
+                this.credits.element.onclick = function() {
+                    window.open(
+                    "http://www.statistik.bs.ch",
+                    '_blank' // http://stackoverflow.com/questions/16810556/how-to-open-credits-url-of-highcharts-in-new-tab
+                    );
+                }
+            }
+        },
+        "borderColor": "#fbfbfb",
+        "backgroundColor": "#fbfbfb",
+        "width": 485,
+        "height": 410,
+        "marginBottom": 65,
+        "marginTop": 30,
+        "style": {
+        "fontFamily": "Arial"
+        },
+        "zoomType": "y",
+        "type": "scatter",
+		"inverted": true
+    },
+    "title": {
+        "style": {
+        "fontSize": 14,
+        "fontWeight": "bold",
+        "fontFamily": "Arial",
+        "color": "#000000"
+        },        
+        "align": "left"
+    },
+    "subtitle": {
+        "style": {
+        "fontSize": 12,
+        "fontWeight": "normal",
+        "fontFamily": "Arial",
+        "color": "#000000"
+        },
+        "text": "",
+        "align": "left"
+    },
+    navigation: {
+        menuItemStyle: {
+            fontFamily: Highcharts.SVGRenderer.prototype.getStyle().fontFamily,
+            padding: '2px 10px'
+        }
+    },
+    "plotOptions": {
+        "series": {
+            "dataLabels": {
+                "x": 5,
+                "align": "left",
+                "verticalAlign": "middle",
+                "enabled": false,
+                "style": {
+                    "fontSize": 10
+                }
+            }
+        }
+    },
+    "yAxis": {
+        "title": {
+            "style": {
+                "color": "#000000",
+                "fontSize": null
+            },
+            "text": null
+        },
+        "labels": {
+            "format": "{value}",
+            "style": {
+                "color": "#000000"
+            }
+        }
+    },
+    "xAxis": {
+        "title": {
+            "style": {
+                "color": "#000000"
+            }
+        },
+        "labels": {
+            "style": {
+                "color": "#000000"
+            }
+        },
+        "tickColor": "#FFFFFF",
+        "type": "category"        
+    },
+    tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+        shared: false
+    },
+    "exporting": {
+        "sourceWidth": null,
+        "scale": 5,
+        buttons: {
+            contextButton: {
+            menuItems: Highcharts.getOptions().exporting.buttons.contextButton.menuItems.slice(0, 8)
+            }
+        }
+    },
+    "credits": {
+        "enabled": true,
+        "style": {
+            "color": "#000000",
+            "fontSize": 10
+        },
+        "position": {
+            "align": "left",
+            "verticalAlign": "bottom",
+            x: 10,
+            y: -17
+        }
+    },
+    "legend": {
+		"enabled": false
+	}
+};
---
+++ charts/sozialhilfequote.js
@@ -0,0 +1,15 @@
+chartOptions['sozialhilfequote'] = {
+	"chart": {
+		"renderTo": "container-sozialhilfequote"
+	},
+	"title": {
+		"text": "Sozialhilfequote 2015"
+	},
+	"credits": {
+		"text": "Quellenangabe: woher kommen die Daten?"
+	},
+	"legend": {
+		"enabled": false
+	},
+	"series": [{}]
+};
