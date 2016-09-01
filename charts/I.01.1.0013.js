e9f8cf4b71572271d454e3c0b7190fdcfee3c5c1
jb3-2 <jonas.bieri@gmail.com>
2016-09-01 16:57:44 +0200

    Reorder legend the same way as data

--- charts/I.01.1.0013.js
+++ charts/I.01.1.0013.js
@@ -34,47 +34,38 @@ chartOptions['I.01.1.0013'] = {
   "series": [
     {
       "color": "#C8C8C8",
-      "legendIndex": 8,
       "index": 0
     },
     {
       "color": "#C5AB92",
-      "legendIndex": 5,
       "index": 3
     },
     {
       "color": "#FF8028",
-      "legendIndex": 7,
       "index": 1
     },
     {
       "color": "#E7CFE2",
-      "legendIndex": 6,
       "index": 2
     },
     {
       "color": "#B9CFD7",
-      "legendIndex": 4,
       "index": 4
     },
     {
       "color": "#FABD24",
-      "legendIndex": 3,
       "index": 5
     },
     {
       "color": "#73BA7C",
-      "legendIndex": 2,
       "index": 6
     },
     {
       "color": "#007A2F",
-      "legendIndex": 1,
       "index": 7
     },
     {
       "color": "#672773",
-      "legendIndex": 0,
       "index": 8
     }        
   ],
