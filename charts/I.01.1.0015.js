08c59fdca85f264f0f35e9e8e157012e2954712d
jb3-2 <jonas.bieri@gmail.com>
2016-09-01 15:45:47 +0200

    Add chart I.01.1.0015

---
+++ charts/I.01.1.0015.js
@@ -0,0 +1,45 @@
+chartOptions['I.01.1.0015'] = {
+  "plotOptions": {
+    "series": {
+      "pointWidth": 15,
+      "dataLabels": {
+        "style": {
+          "fontSize": 10
+        }
+      }
+    },
+    "column": {
+        "colorByPoint": true
+    }
+  },
+  "colors": [
+    "#B00000",
+    "#69929B",
+    "#CD9C00",
+    "#CD9C00",
+    "#CD9C00",
+    "#CD9C00",
+    "#CD9C00",
+    "#CD9C00",
+    "#CD9C00",
+    "#CD9C00",        
+    "#CD9C00"
+  ],  
+  "xAxis": {
+    "type": "category"    
+  },  
+  "legend": {
+    "enabled": false
+  },
+  tooltip: {
+    pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>',
+    shared: false
+  },  
+  "chart": {
+    "renderTo": "container-I.01.1.0015",    
+    "marginBottom": 65,
+    "marginTop": 75,
+    "type": "column",
+    "inverted": true
+  }
+};
