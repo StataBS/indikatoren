af8036ca88e128169a1c510dbadeb5c8ce4cebc0
jb3-2 <jonas.bieri@gmail.com>
2016-09-02 18:09:06 +0200

    Add chart I.01.5.0042

---
+++ charts/I.01.5.0042.js
@@ -0,0 +1,49 @@
+chartOptions['I.01.5.0042'] = {
+  "plotOptions": {
+    "series": {      
+      "dataLabels": {
+        "style": {
+          "fontSize": 10
+        }
+      }
+    },
+  },
+  "xAxis": {
+    "type": "category"    
+  },
+  "yAxis": {
+    "labels": {
+      "format": "{value}"
+    }    
+  },
+  "series": [
+    {
+      "color": "#73BA7C"       
+    },
+    {
+      "color": "#FABD24"
+    }
+  ],
+  "legend": {
+    "enabled": true,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "align": "left",
+    "x": 118,
+		"y": 35,    
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+  tooltip: {
+    pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}</b><br/>',
+    shared: false
+  },  
+  "chart": {      
+    "renderTo": 'container-I.01.1.0017',
+    "marginBottom": 65,
+    "marginTop": 75,
+    "type": "column",
+    "inverted": true
+  }
+};
