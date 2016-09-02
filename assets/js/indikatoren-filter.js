20398e741851e0c853f10976d166d0ff2cd6db8f
jb3-2 <jonas.bieri@gmail.com>
2016-09-02 10:07:51 +0200

    Rename thema contents

--- assets/js/indikatoren-filter.js
+++ assets/js/indikatoren-filter.js
@@ -222,9 +222,11 @@ function configureCascadedControls(level1Selector, level2Selector, level1ValueSe
 };
 
 
-function renderThema(){  
-  var values = ["Alle", "01 Bevölkerung",	"02 Raum, Landschaft, Umwelt",	"03 Erwerbsleben",	"04 Volkswirtschaft",	"05 Preise",	"06 Produktion und Handel",	"07 Land- und Forstwirtschaft",	"08 Energie",	"09 Bau- und Wohnungswesen",	"10 Tourismus",	"11 Verkehr",	"12 Finanzmärkte und Banken",	"13 Soziale Sicherheit",	"14 Gesundheit",	"15 Bildung und Wissenschaft",	"16 Kultur und Sport",	"17 Politik",	"18 Öffentliche Finanzen",	"19 Rechtspflege", "50 Regelmässige Befragungen"];
+function renderThema(){
+    
+  var values = ["Alle", "01 Bevölkerung",	"02 Raum, Umwelt",	"03 Erwerbsleben",	"04 Volkswirtschaft",	"05 Preise",	"06 Produktion, Handel",	"07 Landwirtschaft",	"08 Energie",	"09 Bau-, Wohnungswesen ",	"10 Tourismus",	"11 Verkehr",	"12 Finanzmärkte und Banken",	"13 Soziale Sicherheit",	"14 Gesundheit",	"15 Bildung",	"16 Kultur und Sport",	"17 Politik",	"18 Öffentliche Finanzen",	"19 Rechtspflege", "50 Befragungen"];
 
+  //var values = JsonQuery(Indikatoren).uniq("thema").pluck("thema").all  
   var html = $('#radio-template').html();
   var templateFunction = FilterJS.templateBuilder(html);
   var container = $('#thema_criteria');
