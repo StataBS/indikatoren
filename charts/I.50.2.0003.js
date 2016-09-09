adb3025f15f7174a24c718a07ef60a685706e966
jb3-2 <jonas.bieri@gmail.com>
2016-09-09 16:00:23 +0200

    Add chart I.50.2.0003

---
+++ charts/I.50.2.0003.js
@@ -0,0 +1,27 @@
+chartOptions['I.50.2.0003'] = {
+    "series": [
+        {
+        "color": "#256370"
+        },
+        {
+        "color": "#71A3B5"
+        },
+        {
+        "color": "#FFBB58"
+        },
+        {
+        "color": "#FF8028"
+        },
+        {
+        "color": "#C8C8C8"
+        },
+        {
+        "color": "#8A8A8A"
+        }        
+  ],
+  "xAxis": {
+      "type": "category"
+  },
+};
+
+ 
