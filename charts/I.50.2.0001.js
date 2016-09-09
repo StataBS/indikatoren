ab8ff181b642ce1c71417f71cb806d67b7082da8
jb3-2 <jonas.bieri@gmail.com>
2016-09-09 13:43:29 +0200

    Add chart I 50.2.0001

---
+++ charts/I.50.2.0001.js
@@ -0,0 +1,54 @@
+chartOptions['I.50.2.0001'] = {
+    "xAxis": {
+        "type": "category",
+        "labels": {
+            "useHTML": true
+        }
+    },
+    "yAxis": {
+        "tickInterval": null,
+        "reversedStacks": false
+    },   
+    "series": [
+        {
+        "color": "#256370"
+        },
+        {
+        "color": "#71A3B5"
+        },
+        {
+        "color": "#FFBB58"
+        },
+        {
+        "color": "#FF8028"
+        },
+        {
+        "color": "#C8C8C8"
+        },
+        {
+        "color": "#8A8A8A"
+        }        
+    ],
+    "tooltip": {
+        "pointFormat": '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}%</b><br/>'
+    },
+        
+    "legend": {
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "itemMarginBottom": 5,     
+        "align": "left",
+        "x": 145,
+        "y": 35,
+        "itemWidth": 150,
+        "itemStyle": {
+            "fontWeight": "normal"
+        }
+    },      
+    "chart":
+    {
+        "inverted": true,
+        "marginTop": 110
+    }
+};
+ 
