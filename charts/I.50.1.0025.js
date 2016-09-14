32b67c0b4dcc12dd637b5490c15557eb5295144e
jb3-2 <jonas.bieri@gmail.com>
2016-09-14 16:20:46 +0200

    Add charts I.50.1.0025 and I.50.4.0006

---
+++ charts/I.50.1.0025.js
@@ -0,0 +1,21 @@
+chartOptions['I.50.1.0025'] = {
+    "series": [
+        {
+            "color": "#71A3B5"
+        },
+        {
+            "color": "#FFBB58"
+        },
+        {
+            "color": "#8A8A8A"
+        }        
+  ],
+  "xAxis": {
+      "type": "category"
+  },
+  "legend": {
+      "reversed": false
+  }
+};
+
+ 
