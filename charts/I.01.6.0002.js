c506971781490793854f22c98a08cc22cc4b3549
jb3-2 <jonas.bieri@gmail.com>
2016-09-14 17:59:39 +0200

    Add chart I.01.6.0002

---
+++ charts/I.01.6.0002.js
@@ -0,0 +1,87 @@
+chartOptions['I.01.6.0002'] = {
+  "plotOptions": {
+    "series": {
+      "stacking": "normal",
+      /*"pointWidth": 35,*/
+      "dataLabels": {
+        "style": {
+          "fontSize": 10
+        }
+      }
+    }
+  },
+  "xAxis": {
+    "type": "category"    
+  },  
+  "yAxis": [
+    {
+      "title": {
+          "style": {
+              "color": "#000000",
+              "fontSize": null
+          },
+          "text": null
+      },
+      "labels": {
+          "style": {
+          "color": "#000000"
+          },
+          "format": "{value}",
+      },
+      "reversedStacks": true
+    },
+    {
+      "min": 0,
+      "title": {
+          "style": {
+              "color": "#000000",
+              "fontSize": null
+          },
+          "text": null
+      },      
+      "labels": {
+        "format": "{value:,0f}"      
+      },
+      "opposite": true
+    }
+  ],
+  "tooltip": {    
+    "shared": false,
+    "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.0f}</b><br/>'
+  },  
+  "legend": {
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "itemMarginBottom": 5,
+    "x": 46,
+    "y": 30,
+    "align": "left",
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+  "series": [
+    {
+      "color": "#FABD24",
+      "type": "column", 
+      "legendIndex": 1
+    },
+    {
+      "color": "#008AC3",
+      "type": "column", 
+      "legendIndex": 0
+    },
+    {
+      "color": "#9F7C5A",
+      "type": "line",
+      "yAxis": 1,
+      "stacking": null, 
+      "legendIndex": 4
+    }    
+  ],
+  "chart": {
+    "marginBottom": 65,
+    "marginTop": 150,
+    "type": "column"
+  }
+};
