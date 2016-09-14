f72d10d9800b34d66fb4f47056662379da4ab9f3
jb3-2 <jonas.bieri@gmail.com>
2016-09-14 12:08:44 +0200

    Add chart I.50.1.0013

---
+++ charts/I.50.1.0013.js
@@ -0,0 +1,40 @@
+chartOptions['I.50.1.0013'] = {
+        "series": [
+            {
+                "color": "#007A2F"
+            },
+            {
+                "color": "#73BA7C"
+            },
+            {
+                "color": "#FFBB58"
+            },
+            {
+                "color": "#FF8028"
+            },
+            {
+                "color": "#C8C8C8"
+            }        
+    ],
+    "legend": {
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "itemMarginBottom": 5,     
+        "align": "left",
+        "x": 35,
+        "y": 35,
+        "itemWidth": 212,
+        "itemStyle": {
+            "fontWeight": "normal"
+        }
+    },       
+    "xAxis": {
+        "type": "category"
+    },
+    "chart": {
+        "marginBottom": 75,
+        "marginTop": 120
+    }
+};
+
+ 
