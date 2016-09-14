9db0ad0a4c3b65afbd1323a8545ebb09b2fc43ce
jb3-2 <jonas.bieri@gmail.com>
2016-09-14 13:03:58 +0200

    Add chart I.50.1.0017

---
+++ charts/I.50.1.0017.js
@@ -0,0 +1,31 @@
+chartOptions['I.50.1.0017'] = {
+        "series": [
+            {
+                "color": "#007A2F"
+            },
+            {
+                "color": "#73BA7C"
+            },
+            {
+                "color": "#FFBB58"
+            },
+            {
+                "color": "#FF8028"
+            },
+            {
+                "color": "#C8C8C8"
+            }        
+    ],
+    "xAxis": {
+        "type": "category"
+    },
+    "legend": {
+        "reversed": true
+    },
+    "chart": {
+        "marginBottom": 75,
+        "marginTop": 120
+    }
+};
+
+ 
