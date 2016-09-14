4387c9e6f03b67952ca3e4850e192ab21d83517b
jb3-2 <jonas.bieri@gmail.com>
2016-09-14 17:43:30 +0200

    Add charts

---
+++ charts/I.18.2.0004.js
@@ -0,0 +1,49 @@
+ chartOptions['I.18.2.0004'] = {
+  "plotOptions": {
+    "series": {
+      "dataLabels": {
+        "enabled": true,
+          //display label at first or last point: https://gist.github.com/jeremywrowe/3506869
+          formatter: function() {
+            var last  = this.series.data[this.series.data.length - 1];
+            var first  = this.series.data[0];
+            if (this.point.y === first.y || this.point.y === last.y ) {
+              return Highcharts.numberFormat(this.point.y, 1, ",", " ") + '%';
+            }
+            return "";
+          }
+      }
+    }
+  },
+  "yAxis": {
+    "min": 0,
+    "max": 20,
+    "tickInterval": 5,
+    "labels": {
+      "format": "{value:,.0f}%",
+    },
+  },
+  "xAxis": {
+    "tickInterval": 1
+  },
+  "legend": {
+    "enabled": false,
+    "layout": "vertical",
+    "verticalAlign": "middle",
+    "align": "right"
+  },
+  "series": [
+    {
+    "color": "#FF8028"
+    }
+  ],
+  "tooltip": {
+    "shared": false,
+    "pointFormat": '<span style="color:{series.color}">{series.name}</span>: <b>{point.y:.1f}%</b><br/>',
+  },
+  "chart": {
+    "marginBottom": 65,
+    "marginTop": 75,
+    "type": "column"
+  }
+};
