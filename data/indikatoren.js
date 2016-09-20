4d0953dbdcec1d3fe2a09276386776c56276f0c1
jb3-2 <jonas.bieri@gmail.com>
2016-09-20 16:13:35 +0200

    Add chart I.08.1.0028

--- data/indikatoren.js
+++ data/indikatoren.js
@@ -960,7 +960,7 @@
 "stufe5":"", 
 "lesehilfe":"Der Endenergieverbrauch hat zwischen 2010 und 2014 um 16% auf 4 630 Mio. kWh abgenommen. Der Rückgang verteilt sich gleichmässig auf die Verbrauchergruppen Wohnen und Nichtwohnen und ist primär der milden Witterung geschuldet. Das Jahr 2014 hatte rund 17% weniger Heizgradtage als das Jahr 2012.",
 "erlaeuterungen":"",
-"template":"dotplot",
+"template":"template001",
 "option":"options001",
 "quellenangabe":["Statistisches Amt Basel-Stadt, kantonale Energiestatistik"]
 },
