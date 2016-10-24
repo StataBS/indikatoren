18ff4efeaa591fdb75a60f858837410999472a62
Jonas Bieri <jonas.bieri@gmail.com>
2016-10-24 23:53:16 +0200

    Change title to test travis ci auto-build and deploy to gh-pages

--- metadata/single/I.01.1.0013.js
+++ metadata/single/I.01.1.0013.js
@@ -1,4 +1,4 @@
-﻿var indikatoren = [{"title":"Ausländeranteil nach Staatsangehörigkeit seit 2006 (Test)",
+﻿var indikatoren = [{"title":"Ausländeranteil nach Staatsangehörigkeit seit 2006",
 "subtitle":"",
 "kuerzel":"I.01.1.0013",
 "kuerzelKunde":"1-1",
