e142e7d8d3f22aa6ba99369f8b094704a7ebf2f3
travis-ci.org <travis@testing-worker-linux-docker-c65a6e3e-3459-linux-14.prod.travis-ci.org>
2016-11-03 11:39:19 +0000

    Built from commit a7ae2ca1143816778690180840467dd68f9ea79b

--- metadata/indikatoren.js
+++ metadata/indikatoren.js
@@ -612,8 +612,8 @@ var indikatoren = [
 		"stufe3": "",
 		"stufe4": "",
 		"stufe5": "",
-		"lesehilfe": "Die Haushaltsstruktur weist in den letzten Jahren keine grossen Veränderungen auf. Einpersonen-haushalte machen rund 47% aus.",
-		"erlaeuterungen": "Nichtfamilienhaushalte sind Mehrpersonenhaushalte wie Wohngemeinschaften und unverheiratete Paare ohne ein gemeinsames Kind. Die übrigen Mehrpersonenhalushaltewerden zu den Familienhaushalten gezählt.",
+		"lesehilfe": "Die Haushaltsstruktur weist in den letzten Jahren keine grossen Veränderungen auf. Einpersonenhaushalte machen rund 47% aus.",
+		"erlaeuterungen": "Nichtfamilienhaushalte sind Mehrpersonenhaushalte wie Wohngemeinschaften und unverheiratete Paare ohne ein gemeinsames Kind. Die übrigen Mehrpersonenhaushalte werden zu den Familienhaushalten gezählt.",
 		"template": "template001",
 		"option": "options001",
 		"quellenangabe": [
