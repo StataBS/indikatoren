8376f42b1b34bceab811f42b9d3f54af4d222eda
Jonas Bieri <jonas.bieri@gmail.com>
2016-11-06 14:06:07 +0100

    Add tooltip formatter

--- charts/templates/I.01.1.0038.map.js
+++ charts/templates/I.01.1.0038.map.js
@@ -4,6 +4,11 @@ chartOptions['I.01.1.0038.map'] = {
 	},
 	colorAxis: {
 	},
+	"tooltip": {
+		"pointFormatter": function(){
+			return '<span style="color:{series.color}">' + this.series.data[this.x].name + '</span>: <b>'+Highcharts.numberFormat((this.value*100),2)+'%</b><br/>Rang <b>' + parseInt(this.x + 1) + '</b>'
+		}
+	},
 	series: [{
 		/*data: data,*/
 		mapData: geojson_wohnviertel,
