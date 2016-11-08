40621244a7a68cb9de62e2dc2aae5830c68eb628
jb3-2 <jonas.bieri@gmail.com>
2016-11-08 11:31:15 +0100

    Change color axis and its formatter function (add %)

--- charts/templates/I.01.1.0038.map.js
+++ charts/templates/I.01.1.0038.map.js
@@ -9,12 +9,25 @@ chartOptions['I.01.1.0038.map'] = {
 			return '<span style="color:{series.color}">' + this.series.data[this.x].name + '</span>: <b>'+Highcharts.numberFormat((this.value*100),2)+'%</b><br/>Rang <b>' + parseInt(this.x + 1) + '</b>'
 		}
 	},
+	mapNavigation: {
+		enabled: false,
+		buttonOptions: {
+			verticalAlign: 'bottom'
+		}
+	},
+	colorAxis: {
+		min: 0,
+		labels: {
+			formatter: function () {
+				return Highcharts.numberFormat((this.value*100),0)+'%'; 
+			}
+		}
+	},			
 	series: [{
 		animation: true,
 		mapData: geojson_wohnviertel,
-		joinBy: ['name', 0],
+		joinBy: ['name'],
 		keys: ['name', 'value'],
-		name: 'Random data',
 		states: {
 			hover: {
 				color: '#BADA55'
