87243fd4689d6569e8af687628c5c8e6110795e9
travis-ci.org <travis@travis-ci.org>
2017-03-29 12:03:01 +0000

    Built from commit b8748464b73c1f66ddcaba8fd7653e925cd19468

---
+++ charts/templates/stock001.js
@@ -0,0 +1,123 @@
+/* global Highcharts */
+(function(){
+    return {
+        "isStock": true,
+        "chart": {
+            "borderColor": "#fbfbfb",
+            "backgroundColor": "#fbfbfb",
+            "width": 485,
+            "height": 415,        
+            "spacingBottom": 60,
+            "style": {
+                "fontFamily": "Arial"
+            },
+            "events":{
+                    "load": function() {
+                        this.credits.element.onclick = function() {};
+                    }
+            }
+        },    
+        "plotOptions": {
+            "series": {
+                "dataLabels": {
+                    "style": {
+                    "fontSize": "10px"
+                    }
+                }
+            }
+        },
+        "title": {
+            "style": {
+            "fontSize": "14px",
+            "fontWeight": "bold",
+            "color": "#000000"
+            },
+            "align": "left"
+        },
+        "subtitle": {
+            "style": {
+            "fontSize": "12px",
+            "fontWeight": "normal",
+            "fontFamily": "Arial",
+            "color": "#000000"
+            },
+            "text": "",
+            "align": "left"
+        },
+        "xAxis": {
+            "uniqueNames": true,
+            "ordinal": false,
+            "title": {
+                "style": {
+                    "color": "#000000"
+                }
+            },
+            "labels": {
+                "style": {
+                    "color": "#000000"
+                }
+            },
+            "tickColor": "#FFFFFF"
+        }, 
+        "yAxis": {
+            "title": {
+                "style": {
+                    "color": "#000000",
+                    "fontSize": null
+                },
+                "text": null
+            },
+            "labels": {
+                "y": 3,
+                "style": {
+                    "color": "#000000"
+                }
+            }
+        },    
+    	"navigation": {
+    		"menuItemStyle": {
+    			"fontFamily": Highcharts.SVGRenderer.prototype.getStyle().fontFamily,
+    			"padding": '2px 10px'
+    		}
+    	},
+        "credits": {
+            "enabled": true,
+            "style": {
+                "color": "#000000",
+                "fontSize": "10px",
+                "cursor": "default"
+            },
+            "position": {
+                "align": "left",
+                "verticalAlign": "bottom",
+                "x": 10
+            }
+        },
+        "legend": {
+            "symbolRadius": 0
+        }, 
+        "rangeSelector": {
+            buttonPosition: {
+                x: 10,
+                y: 365
+            }
+        },
+        navigator: {
+            margin: 10, 
+            height: 30,
+            series: {
+                includeInCSVExport: false
+            }
+        }, 
+        tooltip: {
+            dateTimeLabelFormats: {
+                day:"%A, %e. %b, %Y"
+            }
+        },
+        exporting: {
+            csv: {
+                dateFormat: '%d.%m.%Y'
+            }
+        }
+	};
+}());
--- metadata/sets/Wohnraumentwicklung.js
+++ metadata/sets/Wohnraumentwicklung.js
@@ -163,18 +163,17 @@ var indikatorensetData = [
 	{
 		"title": "Bevölkerung am Jahresende",
 		"subtitle": "",
-		"kuerzel": "I.01.1.0023",
-		"kuerzelKunde": "1.1.a",
-		"image": "images/I.01.1.0023.jpg",
+		"kuerzel": "I.01.1.0077",
+		"kuerzelKunde": "1.1.A",
 		"description": "Bevölkerung am Jahresende",
-		"id": "4132",
+		"id": 4132,
 		"thema": "01 Bevölkerung",
 		"unterthema": "Bestand und Struktur ",
 		"kennzahlenset": "Wohnraumentwicklung",
 		"raeumlicheGliederung": [
 			"Kanton"
 		],
-		"aktualisierungsdatum": "",
+		"aktualisierungsdatum": "2017-03-22T18:38:03.033",
 		"schlagwort": [
 			"Heimat",
 			"Bevölkerung",
@@ -187,10 +186,18 @@ var indikatorensetData = [
 		"stufe5": "",
 		"lesehilfe": "Von 2005 bis 2015 ist die Anzahl Einwohnerinnen und Einwohner im Kanton Basel-Stadt um 4,9% angestiegen.",
 		"erlaeuterungen": "Bis zum Jahr 2011 wurde die Bevölkerungszahl durch Fortschreibung ermittelt. Seit dem Jahr 2012 basiert sie direkt auf Auswertungen aus dem kantonalen Einwohnerregister.",
-		"template": "line001",
+		"template": "stock001",
 		"option": "options001",
+		"visible": true,
+		"visibleInPortal": true,
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
 		"quellenangabe": [
 			"Statistisches Amt Basel-Stadt, kantonale Bevölkerungsstatistik"
+		],
+		"externalLinks": [
+			""
 		]
 	},
 	{
