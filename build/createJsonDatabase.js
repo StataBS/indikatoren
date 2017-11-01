49c9d8033c084053cb80192339e64ef1fb7c04a8
Jonas Bieri <jb3-2@users.noreply.github.com>
2017-11-01 20:30:37 +0100

    Issue 402 (#127)
    * Hide Umwelt charts in portal view that only have incomplete Erlaeuterungen, see #402
    * Hide Umwelt charts from Portal view for the moment, see #402

--- build/createJsonDatabase.js
+++ build/createJsonDatabase.js
@@ -17,9 +17,15 @@ files.forEach(function(filepath){
     if (indikator.visible == undefined || indikator.visible == true) {
         console.log(filepath + ' is visible, proceeding with adding...');
         if (indikator.visibleInPortal == undefined || indikator.visibleInPortal == true) {
-            console.log(filepath + ' is visibleInPortal, proceeding with adding to portal/indikatoren.json, all/indikatoren.json...');
-            indikatoren.push(indikator);
             indikatorenInSet.push(indikator);
+            //Special case for Umwelt charts: Hide from Portal view //, if no complete text in field erlaeuterungen
+            if (indikator.kennzahlenset == 'Umwelt'/* && indikator.erlaeuterungen.startsWith('Eine detaillierte Beschreibung des Indikators') */){
+                console.log(filepath + ' is Umwelt Indikator ' /*+ 'with no complete erlaeuterungen'*/ + ', thus hiding in portal view');
+            }
+            else {
+                console.log(filepath + ' is visibleInPortal, proceeding with adding to portal/indikatoren.json, all/indikatoren.json...');
+                indikatoren.push(indikator);
+            }
         }
         else {
             console.log(filepath + ' is NOT visibleInPortal, ignoring for portal/indikatoren.json but adding to all/indikatoren.json');
