1f94ee9fe14b0a618ea65898c02782bd80ba5dbc
Jonas Bieri <jb3-2@users.noreply.github.com>
2017-11-30 12:03:37 +0100

    Fix xAxis label by assigning correct template, close #478 (#150)

--- metadata/all/templatesById.js
+++ metadata/all/templatesById.js
@@ -265,7 +265,7 @@ var templatesById = {
 	"5817": "template001",
 	"5818": "mappie001",
 	"5819": "befragungen001",
-	"5821": "befragungen001",
+	"5821": "befragungenProzent001",
 	"5822": "pie001",
 	"5823": "line001",
 	"5825": "mappie001",
