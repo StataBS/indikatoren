a5e0944db371d3ef131b9d713e8e96fcdfb7c31c
travis-ci.org <travis@travis-ci.org>
2017-12-07 10:02:24 +0000

    Built from commit 4bbaed17b5606d4893ffe14336c25570399a777e

--- charts/templates/9999.js
+++ charts/templates/9999.js
@@ -1,43 +1,28 @@
-/*  
-	global Highcharts
-	global geojson_wohnviertelEPSG2056 
-	global $
+/* 
+global Highcharts
+global geojson_wohnviertelEPSG2056 
 */
 (function(){
-
     return {
-    	"legend": {
-			"title": {
-				"text": ""
-			}
-			},
 		"colorAxis": {
-			//"min": undefined,
-			"minColor": "#eff6e9",
-			"maxColor": "#4b7b1f",
+			"minColor": "#f4ebf3",
+			"maxColor": "#5b2659",
 			"labels": {
 				"formatter": function () {
-					return Highcharts.numberFormat((this.value),1); 
+					return Highcharts.numberFormat((this.value/1000),0); 
 				}
+			}
+		},
+		"legend": {
+			"title": {
+				"text": "in tausend Einheiten"
 			}
 		},
         "data": {
 		    "seriesMapping": [
 		      {
 		      	x: 0, y: 2
-		      },
-		      {
-		      	//2nd series: use y values from column 3
-		      	y: 3
-		      },
-		      {
-		      	x: 0, 
-		      	y: 4
-		      },
-		      {
-		      	x: 0, 
-		      	y: 5
-		      }
+		      }		      
 		    ]
         },
 		"series": [
@@ -46,101 +31,16 @@
 				"animation": true,
 				"mapData": geojson_wohnviertelEPSG2056,
 				"borderColor": "#fbfbfb",		
-				"joinBy": ['TXT', 'Wohnviertel_Id'],
-				"keys": ['Wohnviertel_Id', 'value'],
+				"joinBy": ['TXT', 'Wohnviertel Id'],
+				"keys": ['Wohnviertel Id', 'value'],
 				"states": {
 					"hover": {
 						"enabled": false,
 						"borderColor": '#BADA55',
 						"brightness": 0
 					}
-				}, 
-				tooltip: {
-					pointFormatter: function(){
-						//console.log(this);
-						return this.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.value),3) + '</b><br/>';
-					}
 				}
-			}, 
-			{
-				"visible": false
-			}, 
-			{
-				visible: false,
-				type: 'pie',
-				color: 'red',
-				borderColor: 'red'
-
-			}, 
-			{
-				visible: false,
-				type: 'pie',
-        		color: 'blue',
-        		borderColor: 'blue'
-			}
-		],
-		chart: {
-			events: {
-	            load: function (e) {
-	            	
-	            	this.credits.element.onclick = function() {};
-
-	                var chart = this;
-	                var fn = this.options.customFunctions;
-	                //define new Highcharts template "mappie"
-					fn.defineTemplate();
-					
-					var choroplethSeries = chart.series[0];
-					var pieSizeSeries = chart.series[1];
-					var pieSeries = [chart.series[2], chart.series[3]];
-
-					//pie diameters in px
-					var maxPieDiameter = 22;
-
-					var extremeValues = fn.getPointsExtremes(pieSizeSeries.points);
-
-					//define chart-specific details
-					var pieSeriesConfig = function(data, correspondingMapSeriesItem, color){
-						return {
-	                        sizeFormatter: function () {
-	                            var fn = this.chart.options.customFunctions;
-	                            var yAxis = chart.yAxis[0], zoom = (yAxis.dataMax - yAxis.dataMin) / (yAxis.max - yAxis.min);
-								return zoom * fn.pieSize(Math.abs(data.value), fn.getPointsExtremes(pieSizeSeries.points).maxAbsNumber, maxPieDiameter); 
-								//return fn.pieSizeCategorical(Math.abs(data.value), pieSizeCatConfig).diameter;
-	                        },
-	                        tooltip: {
-	                        	headerFormat: '<span style="color:{point.color}">\u25CF</span> <span style="font-size: 10px"> {point.key} </span><br/>',
-	                            pointFormatter: function () {
-	                            	return correspondingMapSeriesItem.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.v),3) + '</b><br/>';
-	                            }
-	                        },
-	                    };
-					};
-					var pieSizeCatConfig;
-					//put the pies / bubbles on the map
-					fn.drawPies(chart, pieSizeSeries, pieSeries, choroplethSeries, pieSeriesConfig, pieSizeCatConfig);
-	                
-					//pie values in legend
-	                var minValueInLegend = 0.001; 
-	                var maxValueInLegend = 0.1; 
-	                
-                	//Add manually drawn legend	
-	                fn.addLegendTitle(chart, pieSizeSeries.name, 285, 240);
-	                
-	                fn.addLegendCircle(chart, 410, 275, 0.5*fn.pieSize(minValueInLegend, extremeValues.maxAbsNumber, maxPieDiameter), 'grey', 'pieLegendHideOnZoom');
-	                fn.addLegendLabel(chart, Highcharts.numberFormat((minValueInLegend),3,","," "), 430, 265, 'pieLegendHideOnZoom');
-	                fn.addLegendCircle(chart, 410, 300, 0.5*fn.pieSize(maxValueInLegend, extremeValues.maxAbsNumber, maxPieDiameter), 'grey', 'pieLegendHideOnZoom');
-	                fn.addLegendLabel(chart, Highcharts.numberFormat((maxValueInLegend),2,"."," "), 430, 290, 'pieLegendHideOnZoom');
-
-					fn.addLegendSquare(chart, 290, 270, 10, 'red');
-					fn.addLegendLabel(chart, 'Benzin', 310, 265);
-					fn.addLegendSquare(chart, 290, 295, 10, 'blue');
-					fn.addLegendLabel(chart, 'Diesel', 310, 290);
-					
-					//make sure pies are hidden upon click onto pie legend
-					fn.AddPieLegendClickHandler(chart);
-	            }
-			}
-		}
+			}	
+		], 
 	};
 }());
