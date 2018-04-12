1dbe19a31ff8e1e510d9748cf3175373d52565cc
travis-ci.org <travis@travis-ci.org>
2018-04-12 09:07:27 +0000

    Built from commit 7030515a2e9af78282b3006ff9f5ae5cb224ea31

---
+++ charts/templates/9996.js
@@ -0,0 +1,14 @@
+(function(){
+    return {
+      series: [
+        {
+        }
+      ], 
+      data: {
+		    seriesMapping: [
+		        {x: 5, y: 9/*, Wert: 4*/}
+        ],  
+      }, 
+    };
+}());
+
