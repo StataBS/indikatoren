fa2c06501f977b119b78f5cfddcd6851ebfb847a
Jonas Bieri <jb3-2@users.noreply.github.com>
2018-05-07 17:40:56 +0200

    Spider: One tsv for all, filter on data parsed (#222)
    * Change spider git status
    
    * Enhance spider so that all data can be added and data is filtered after parsing, close #853
    
    * Rebuild

--- charts/templates/9996.js
+++ charts/templates/9996.js
@@ -8,6 +8,22 @@
 		    seriesMapping: [
 		        {x: 5, y: 9/*, Wert: 4*/}
         ],  
+        parsed: function(columns){
+          //define which wohnviertel to display
+          var filterValue = 1;
+          
+          //define which column (zero-based index) should be filtered with above value 
+          var filterColumnIndex = 7;
+          //go backwards through the column with the values to filter (because we remove values at the end of the array and want to retain the index)
+          for (var i = columns[0].length; i > 0; i--){
+            if (columns[filterColumnIndex][i] != filterValue){
+              //go through all columns and remove the value at the current position
+              for (var j=0; j < columns.length; j++){
+                columns[j].splice(i, 1);
+              }
+            }
+          }
+        }
       }, 
     };
 }());
