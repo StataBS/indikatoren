9febd48526e585cca8de6fd28e3e5d6c4ae8966b
travis-ci.org <travis@travis-ci.org>
2018-09-25 11:33:45 +0000

    Built from commit 73d1e29b71e2a3310d7c231db669aa93660dd0fa

--- charts/templates/6164.js
+++ charts/templates/6164.js
@@ -13,25 +13,28 @@
 	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>'
   },
   "series": [
-    {"color": "#CD9C00",
-      "marker": {
-        "enabled": true
-      }    
-    }, /* gelb */
-    	{"color": "#3C3C3C",
-	visible:true,
-      "marker": {
-        "enabled": false
-      },
-       dashStyle: 'LongDash'
-    }, /* schwarz */
+    { /* gelb */
+      "color": "#CD9C00"
+    },
+    { /* schwarz */
+      "color": "#3C3C3C",
+      dashStyle: 'ShortDash',
+      marker: {
+        enabled: false, 
+        states: {
+          hover: {
+            enabled: false
+          }
+        }
+      }
+    },
   ],
   "legend": {
-  	symbolWidth: 30,
+  	//symbolWidth: 30,
     "enabled": true,
     //"x": 45,
     //"y": 35,
-    "itemWidth": 500,
+    //"itemWidth": 500,
     "layout": "horizontal",
     "verticalAlign": "top",
     "itemMarginBottom": 5,
