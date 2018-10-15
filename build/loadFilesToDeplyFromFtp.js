9a1cb4fc1d481f554f0cd3c62ad8422bf186c771
Jonas Bieri <jonas.bieri@gmail.com>
2018-10-15 08:09:35 +0000

    Also get tsv from dev branch upon deployment, close #1121

--- build/loadFilesToDeplyFromFtp.js
+++ build/loadFilesToDeplyFromFtp.js
@@ -1,5 +1,3 @@
-
-
 const fs = require("fs");
 const https = require("https");
 const filePath = "tmp/chartsToDeploy.txt";
@@ -32,14 +30,13 @@ https.get(urlBase + "verzeichnis.txt", listOfCharts => {
             console.log('downloading files for '+ row.Indikator + '...');
             downloadFileContents(urlBase + row.Indikator + '.json', 'metadata/single/' + row.Indikator + '.json');
             downloadFileContents(urlBase + row.Indikator + '.tsv', 'data/' + row.Indikator + '.tsv');
-            //checkout js for each chart with a branch number other than 0
             
+            //checkout js and overwrite tsv for each chart with a branch number other than null
             if (row.Branch){
                 console.log('checking out ' + row.Indikator + '.js from branch ' + row.Branch + '...');
                 child_process.exec('git checkout origin/issue-' + row.Branch + ' -- charts/templates/' + row.Indikator + '.js');
-                //Git.exec('git checkout origin/issue-' + row.Branch + ' -- charts/templates/' + row.Indikator + '.js');
+                child_process.exec('git checkout origin/issue-' + row.Branch + ' -- data/' + row.Indikator + '.tsv');
             }
-        
         });
         console.log('...done!');
   });
