606ee8e8a6617eafaa873da2ea0b297e89ccbafc
travis-ci.org <travis@travis-ci.org>
2018-10-18 15:38:02 +0000

    Built from commit e846582d137d67b2e5257b8f06b5d091a136f79f

--- charts/templates/4868.js
+++ charts/templates/4868.js
@@ -1,5 +1,21 @@
 (function(){
     return {
+  "plotOptions": {
+    "series": {
+      "dataLabels": {
+        "enabled": false,
+          //display label at first or last point: https://gist.github.com/jeremywrowe/3506869
+          formatter: function() {
+            var last  = this.series.data[this.series.data.length - 1];
+            var first  = this.series.data[0];
+            if (this.point.x === first.x || this.point.x === last.x) {
+              return Highcharts.numberFormat(this.point.y, 1, ",", " ");
+            }
+            return "";
+          }
+      }
+    }
+  },
   "yAxis": {
     "labels": {
       "format": "{value:,.0f}",
@@ -17,12 +33,11 @@
     }
   ],
   "tooltip": {
+    valueDecimals: 1,
     "shared": false
   },
   "chart": {
-    "marginBottom": 65,
-    "marginTop": 75,
     "type": "column"
   }
-	}
+	};
 }());
