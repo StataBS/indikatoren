ba963f7cdc3c10e50d1e15f542bf55f60c7c3ad1
mathiasbestgen <42381469+mathiasbestgen@users.noreply.github.com>
2018-12-05 12:10:56 +0100

    Update c9-setup.sh

--- c9-setup.sh
+++ c9-setup.sh
@@ -29,5 +29,5 @@ sudo apt-get update
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt-get install ttf-mscorefonts-installer
 
-npm install 
+#npm install 
 #npm run build
