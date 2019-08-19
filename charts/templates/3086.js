a490aac52c059f80055eb192570ef1394500daed
dmathys <daniel.mathys@bs.ch>
2019-08-19 10:40:28 +0200

    Live schalten, 3086 zurücksetzen

--- charts/templates/3086.js
+++ charts/templates/3086.js
@@ -1,47 +1,47 @@
 (function(){
-    return {
-   plotOptions: {
-        series: {
-           //pointPadding: 0.1,
-            //borderWidth: 0,
-			//"pointWidth": 25,
- 
+  return {
+ plotOptions: {
+      series: {
+          pointPadding: 0,
+          borderWidth: 0,
+    "pointWidth": 40,
 
-        }
-   },
-  "xAxis": {
-    "type": "category",
-    "labels": {
-      "rotation": 0 
-    } 
-  },
-  "yAxis": {
-	//"max": 100,
-    "labels": {
-      "format": "{value:,.0f}"
-    }    
-  },
-  "series": [
-  {"color": "#CD9C00", "visible": true}, /* hellbraun*/
-  //{"color": "#83522E", "visible": true} /* dunkelbraun */
-  ],
-  "legend": {
-    "enabled": false,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
-  },
-   tooltip: {
-    headerFormat: ' ',
-    pointFormat: '<span style="color:{point.color}">\u25CF</span> {point.name}: <b>{point.y:,.0f}</b><br/>',
-    "shared": false
-  },
-  "chart": {      
-    "type": "column",
-    "inverted": false,
-    //"spacingTop": 100
+
+      }
+ },
+"xAxis": {
+  "type": "category",
+  "labels": {
+    "rotation": 0 
+  } 
+},
+"yAxis": {
+//"max": 100,
+  "labels": {
+    "format": "{value:,.0f}"
+  }    
+},
+"series": [
+{"color": "#CD9C00", "visible": true}, /* hellbraun*/
+//{"color": "#83522E", "visible": true} /* dunkelbraun */
+],
+"legend": {
+  "enabled": false,
+  "layout": "horizontal",
+  "verticalAlign": "top",
+  "itemStyle": {
+    "fontWeight": "normal"
   }
+},
+ tooltip: {
+  headerFormat: ' ',
+  pointFormat: '<span style="color:{point.color}">\u25CF</span> {point.name}: <b>{point.y:,.0f}</b><br/>',
+  "shared": false
+},
+"chart": {      
+  "type": "column",
+  "inverted": false,
+  //"spacingTop": 100
+}
 }
 }());
--- metadata/all/indikatoren.js
+++ metadata/all/indikatoren.js
@@ -23459,7 +23459,7 @@ var indikatoren = [
 	},
 	{
 		"title": "Ausländer nach Staatsangehörigkeit",
-		"subtitle": "nach Gemeinde und Wohnviertel, 2017",
+		"subtitle": "nach Gemeinde und Wohnviertel, 2018",
 		"kuerzel": "I.01.1.6399",
 		"kuerzelKunde": "A1-10",
 		"description": "Ausländer nach Staatsangehörigkeit",
@@ -23469,13 +23469,13 @@ var indikatoren = [
 		"raeumlicheGliederung": [
 			"Wohnviertel"
 		],
-		"aktualisierungsdatum": "2018-10-03T09:50:02.022",
+		"aktualisierungsdatum": "2019-08-16T09:50:27.2727",
 		"stufe1": "Kapitel A Bevölkerungsstruktur",
 		"stufe2": "",
 		"stufe3": "",
 		"stufe4": "",
 		"stufe5": "",
-		"lesehilfe": "In fast allen Gemeinden und Wohnvierteln bilden im Jahr 2017 entweder Deutsche oder Personen aus der EU-17 + EFTA die grösste Ausländergruppe. Eine Ausnahme stellt das Wohnviertel Kleinhüningen dar, wo Personen aus dem Balkan die grösste Ausländergruppe stellen.",
+		"lesehilfe": "In fast allen Gemeinden und Wohnvierteln bilden im Jahr 2018 entweder Deutsche oder Personen aus der EU-17 + EFTA die grösste Ausländergruppe. Eine Ausnahme stellt das Wohnviertel Kleinhüningen dar, wo Personen aus dem Balkan die grösste Ausländergruppe stellen.",
 		"erlaeuterungen": "",
 		"template": "template001",
 		"darstellungsart": "Grafik",
@@ -23490,14 +23490,18 @@ var indikatoren = [
 		"quellenangabe": [
 			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
 		],
+		"datenInChartIntegriert": false,
 		"externalLinks": [
 			""
 		],
+		"data-id": 6399,
+		"chart-id": 6399,
+		"filter": "",
 		"id": 6399
 	},
 	{
 		"title": "Anteil Ausländer der Ländergruppe EU-17 + EFTA",
-		"subtitle": "nach einzelnen Staatsangehörigkeiten, Basel-Stadt, 2017",
+		"subtitle": "nach einzelnen Staatsangehörigkeiten, Basel-Stadt, 2018",
 		"kuerzel": "I.01.1.6400",
 		"kuerzelKunde": "A1-6",
 		"description": "Anteil Ausländer der Ländergruppe EU-17 + EFTA",
@@ -23507,13 +23511,13 @@ var indikatoren = [
 		"raeumlicheGliederung": [
 			"Kanton"
 		],
-		"aktualisierungsdatum": "2018-10-03T09:50:02.022",
+		"aktualisierungsdatum": "2019-08-16T09:56:46.4646",
 		"stufe1": "Kapitel A Bevölkerungsstruktur",
 		"stufe2": "",
 		"stufe3": "",
 		"stufe4": "",
 		"stufe5": "",
-		"lesehilfe": "Von 15 433 Personen, die Ende 2017 zur Kategorie «EU-17 + EFTA» zählten, bildeten Spanier, Portugiesen und Briten die grössten Bevölkerungsgruppen.",
+		"lesehilfe": "Ende 2018 zählten 15 844 Personen zur Kategorie «EU-17 + EFTA». Innerhalb dieser Gruppe bildeten Spanier und Portugiesen die grössten Bevölkerungsgruppen.",
 		"erlaeuterungen": "",
 		"template": "pie001",
 		"darstellungsart": "Grafik",
@@ -23528,9 +23532,13 @@ var indikatoren = [
 		"quellenangabe": [
 			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
 		],
+		"datenInChartIntegriert": false,
 		"externalLinks": [
 			""
 		],
+		"data-id": 6400,
+		"chart-id": 6400,
+		"filter": "",
 		"id": 6400
 	},
 	{
