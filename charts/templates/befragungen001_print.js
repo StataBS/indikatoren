484329603d789ee2298def2a3c68e7389a3f7e37
bl99 <ssslib@bs.ch>
2019-10-22 17:58:27 +0200

    Update template befragung001_print.js (tested in 1504 for set Integration (Print)). Changes in template allow to remove a lot of unnecessary code from indicator-JS-files

--- charts/templates/befragungen001_print.js
+++ charts/templates/befragungen001_print.js
@@ -1,159 +1,152 @@
-(function(){
+(function () {
     return {
-    "chart": {		
-        "events":{
-            "load": function() {
-                this.credits.element.onclick = function() {};
-                //square legends must be placed 3 pixels more to the left that lines, don't know why
-                var squareLegendX = (this['options']['chart']['type'] == 'line' ? 0 : 3);
-                
-              //for top-left legends with no x defined: move legend to x position of first yAxis
-              if (this['legend']['options']['align'] == 'left' && this['legend']['options']['verticalAlign'] == 'top' && this['legend']['options']['x'] == 0){
-                this.update(
-                  {
-                    legend: {
-                      x: this.yAxis[0].left - this.spacingBox.x - this.legend.padding - squareLegendX
+        chart: {
+            events: {
+                load: function () {
+                    this.credits.element.onclick = function () { };
+
+                    //square legends must be placed 3 pixels more to the left than lines, don't know why
+                    var squareLegendX = (this['options']['chart']['type'] == 'line' ? 0 : 3);
+
+                    //for top-left legends with no x defined: move legend to x position of first yAxis
+                    if (this['legend']['options']['align'] == 'left' && this['legend']['options']['verticalAlign'] == 'top' && this['legend']['options']['x'] == 0) {
+                        this.update({
+                            legend: {
+                                x: this.yAxis[0].left - this.spacingBox.x - this.legend.padding - squareLegendX
+                            }
+                        });
                     }
-                  }
-                );
-              }                              
-            }
-        },
-        "borderColor": "#fbfbfb",
-        "backgroundColor": "#fbfbfb",
-       	"width": 320,
-    	"height": 208, 
-    	spacing: [2,2,2,2], /*top, right, bottom and left */
-         "style": {
-			"fontFamily": "Arial",
-			"fontSize": "10px",
-        },
-        "zoomType": "xy",
-        "type": "column"
-    },    
-    "title": {
-        "style": {
-			"fontSize": "10px",
-			"fontWeight": "normal",
-			"color": "#000000"
-        },        
-        "align": "left"
-    },
-    "subtitle": {
-        "style": {
-			"fontSize": "12px",
-			"color": "#000000"                      
-        },
-        "align": "left"
-    },
-    "navigation": {
-        "menuItemStyle": {
-            "fontFamily": Highcharts.SVGRenderer.prototype.getStyle().fontFamily,
-            "padding": '2px 10px'
-        }
-    },
-    "plotOptions": {
-        "series": {
-            borderWidth: 0,
-            "dataLabels": {
-                "style": {
-                    "fontSize": "10px"
                 }
             },
-            "stacking": "percent"
-        }
-    },
-    "yAxis": {
-        "tickInterval":10,
-        gridLineColor: '#B9CFD7', 
-        gridLineWidth: 0.5,
-        lineColor: '#B9CFD7', 
-        "title": {
-            "style": {
-            "color": "#000000",
-            "fontSize": 10
+            type: "column",
+            width: 320,
+            height: 208,
+            spacing: [2, 2, 2, 2], /*top, right, bottom and left */
+            borderColor: "#fbfbfb",
+            backgroundColor: "#fbfbfb",
+            zoomType: "xy",
+            style: {
+                fontFamily: "Arial",
+                fontSize: "10px"
             },
-            "text": ''
         },
-        "labels": {
-            y: 3, 
-            "format": "{value}%",
-            "style": {
-            "color": "#000000",
-           "fontSize": "10px"
-            }
-        }
-    },
-    "xAxis": {    
-        lineColor: '#B9CFD7', 
-        lineWidth: 0.5,
-        "type": "category",
-        "uniqueNames": true,
-        "tickLength": 0,
-        "title": {
-            "style": {
-				"color": "#000000"
-            }
-        },
-        "labels": {
-            "rotation": 0,
-            "style": {
-                "color": "#000000",
-                "width": 1,
-                 whiteSpace: 'nowrap', 
-                "textOverflow": "none",
-                "fontSize": "10px",
+        plotOptions: {
+            series: {
+                stacking: "percent",
+                borderWidth: 0,
+                dataLabels: {
+                    style: {
+                        fontSize: "10px"
+                    }
+                }
             },
-              "formatter": function() {
-                //add sum of observations of visible series to the axis label
-                var allVisibleSeries = this.chart.series.filter(function(val, i, arr){
-                    return val.visible;
-                });
-                var indexOfCurrentValue = this.axis.names.indexOf(this.value);
-                var sum = allVisibleSeries.reduce(function(accumulator, series, index, arr){
-                    return accumulator + series.yData[indexOfCurrentValue];
-                }, 0);
-                //use N if all series are visible, otherwise use n
-                var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n='; 
-                var formattedSum = Highcharts.numberFormat(sum, 0, ",", " ")
-            	return this.value.replace(" ", "<br/>") + '<br/>(' + nString + sum + ')';
+            column: {
+                groupPadding: 0,
+            }
+        },
+        title: {
+            text: undefined
+        },
+        subtitle: {
+            text: undefined
+        },
+        yAxis: {
+            type: "category",
+            tickInterval: 10,
+            lineColor: '#B9CFD7',
+            gridLineColor: '#B9CFD7',
+            gridLineWidth: 0.5,
+            title: {
+                text: null
+            },
+            labels: {
+                y: 3,
+                format: "{value}%",
+                style: {
+                    fontSize: "10px",
+                    color: "#000000",
+                    textOverflow: "none"
+                }
+            }
+        },
+        xAxis: {
+            type: "category",
+            reversed: true,
+            uniqueNames: true,
+            lineColor: '#B9CFD7',
+            lineWidth: 0.5,
+            tickLength: 0,
+            title: {
+                style: {
+                    color: "#000000"
+                }
+            },
+            labels: {
+                rotation: 0,
+                style: {
+                    fontSize: "10px",
+                    color: "#000000",
+                    width: 1,
+                    whiteSpace: 'nowrap',
+                    textOverflow: "none"
+                },
+                formatter: function () {
+                    //add sum of observations of visible series to the axis label
+                    var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
+                        return val.visible;
+                    });
+                    var indexOfCurrentValue = this.axis.names.indexOf(this.value);
+                    var sum = allVisibleSeries.reduce(function (accumulator, series, index, arr) {
+                        return accumulator + series.yData[indexOfCurrentValue];
+                    }, 0);
+
+                    //use N if all series are visible, otherwise use n
+                    var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n=';
+
+                    //if chart is inverted then add linebreak in xAxis labels before (N=XY), else space
+                    var doBr = (this.chart.inverted == true) ? ' ' : '<br/>'; 
+
+                    //var formattedSum = Highcharts.numberFormat(sum, 0, ",", " ")
+
+                    this.value = this.value.replace("und weitere", "u.a.");
+
+                    //check for value that contains only spaces
+                    return (this.value.replace(/\s/g, "") == "") ? this.value : this.value + doBr + '(' + nString + sum + ')';
+                }
+            }
+        },
+        legend: {
+            enabled: true,
+            useHTML: true,
+            layout: "vertical",
+            align: "right",
+            verticalAlign: "middle",
+            symbolRadius: 0,
+            padding: 0,
+            itemMarginBottom: 4,
+            itemStyle: {
+                fontFamily: "Arial",
+                fontSize: "10px",
+                fontWeight: "normal",
+                textOverflow: null,
+                whitespace: "nowrap"
+            },
+            labelFormatter: function () {
+                return this.name.replace('/ ', '/<br/>');
+            }
+        },
+        tooltip: {
+            pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+            shared: false
+        },
+        credits: {
+            enabled: true,
+            position: {}
+        },
+        navigation: {
+            buttonOptions: {
+                enabled: false
             }
         }
-    },
-    "tooltip": {
-        "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
-        "shared": false
-    },
-    "credits": {
-        "enabled": true,
-        "style": {
-            "color": "#000000",
-            "fontSize": "10px",
-            "cursor": "default"
-        },
-        "position": {
-            "align": "left",
-            "verticalAlign": "bottom",
-            "x": 10
-        }
-    },
-    "legend": {
-        padding: 0,
-        itemMarginBottom: 2,
-        "layout": "vertical",
-        "verticalAlign": "middle",
-        //"itemMarginBottom": 5,     
-        "align": "right",
-        "useHTML": true,
-        "itemStyle": {
-        	fontSize: "10px",
-        	fontFamily: "Arial",
-            fontWeight: "normal",
-        },
-        "symbolRadius": 0,
-        "labelFormatter": function () {
-            return this.name.replace('/ ', '/<br/>');
-        }
-    }
-	};
+    };
 }());
