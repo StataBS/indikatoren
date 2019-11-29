8d7baa4e162c93e18596c9bb3819b1377b15962b
dmathys <daniel.mathys@bs.ch>
2019-11-29 08:35:12 +0100

    Live schalten

--- charts/templates/5182.js
+++ charts/templates/5182.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
   return {
     plotOptions: {
       scatter: {
@@ -8,13 +8,15 @@
       },
     },
     "yAxis": {
+      max: 50,
+      "tickInterval": 10,
       "labels": {
         "format": "{value:,.0f}%",
       }
     },
     "xAxis": {
       "type": "category",
-     //"tickInterval": 1
+      "tickInterval": 1
     },
     "legend": {
       "enabled": true,
@@ -24,25 +26,48 @@
       //"width": 50,
       //"y": 50,
       //"x": 40,
-      "itemMarginBottom": 5,
+      "itemMarginBottom": 2,
       itemWidth: 200,
+      y: -10,
+      margin:3,
       "itemStyle": {
-        "fontWeight": "normal"
-      }
+        "fontWeight": ["bold", "normal", "bold"]
+      },
+      labelFormatter: function () {
+        return this.name.replace(' Mädchen', '').replace(' Knaben','').replace('Knaben','<b>Knaben</b>').replace('Mädchen','<b>Mädchen</b>');
+      }    
     },
     "series": [
-      {"color": "#E64900", legendIndex: 0, "visible": false, marker: {symbol: 'diamond'}},
-      {"color": "#E64900", legendIndex: 1, "visible": false }, // hellrot 
-      {"color": "#008AC3", legendIndex: 2, "visible": false, marker: {symbol: 'diamond'}},
-      {"color": "#008AC3", legendIndex: 4, "visible": false },// hellblau
-      {"color": "#68AB2B", legendIndex: 4, "visible": true, marker: {symbol: 'diamond'}},
-      {"color": "#68AB2B", legendIndex: 5, "visible": true }, //hellgrün 
-      {"color": "#B375AB", legendIndex: 6, "visible": true, marker: {symbol: 'diamond'}},
-      {"color": "#B375AB", legendIndex: 7, "visible": true }, //hellviolett
-      {"color": "#FFDA80", legendIndex: 8, "visible": true, marker: {symbol: 'diamond'}},
-      {"color": "#FFDA80", legendIndex: 9, "visible": true }, // hellgelb 
-      {"color": "#3C3C3C", legendIndex: 10,"visible": true, marker: {symbol: 'diamond'}}, 
-      {"color": "#3C3C3C", legendIndex: 11,"visible": true } // grau
+      {
+        "color": "rgba(255, 255, 255, 0)", legendIndex: 0, "visible": true, marker: { symbol: 'diamond' },
+        events:
+        {
+          legendItemClick: function (e) {
+            e.preventDefault()
+          }
+        }
+      },
+      {
+        "color": "rgba(255, 255, 255, 0)", legendIndex: 1, "visible": true,
+        events:
+        {
+          legendItemClick: function (e) {
+            e.preventDefault()
+          }
+        }
+      },
+      { "color": "#E64900", legendIndex: 2, "visible": false, marker: { symbol: 'diamond' } },
+      { "color": "#E64900", legendIndex: 3, "visible": false }, // hellrot 
+      { "color": "#008AC3", legendIndex: 4, "visible": false, marker: { symbol: 'diamond' } },
+      { "color": "#008AC3", legendIndex: 5, "visible": false },// hellblau
+      { "color": "#68AB2B", legendIndex: 6, "visible": true, marker: { symbol: 'diamond' } },
+      { "color": "#68AB2B", legendIndex: 7, "visible": true }, //hellgrün 
+      { "color": "#B375AB", legendIndex: 8, "visible": true, marker: { symbol: 'diamond' } },
+      { "color": "#B375AB", legendIndex: 9, "visible": true }, //hellviolett
+      { "color": "#FFDA80", legendIndex: 10, "visible": true, marker: { symbol: 'diamond' } },
+      { "color": "#FFDA80", legendIndex: 11, "visible": true }, // hellgelb 
+      { "color": "#3C3C3C", legendIndex: 12, "visible": true, marker: { symbol: 'diamond' } },
+      { "color": "#3C3C3C", legendIndex: 13, "visible": true } // grau
     ],
     "chart": {
       type: 'scatter',
@@ -50,14 +75,14 @@
     },
     tooltip: {
       headerFormat: '<span style="font-size: 10px"> {point.key}</span><br/>',
-      pointFormat: '<span style="color:{point.color}">●</span> {series.name}: <b>{point.y}%</b><br/>'    
-    }  	    
-/*    
-    "marginBottom": 65,
-    "type": "line",
-    "alignTicks": false
-*/    
-};
+      pointFormat: '<span style="color:{point.color}">●</span> {series.name}: <b>{point.y}%</b><br/>'
+    }
+    /*    
+        "marginBottom": 65,
+        "type": "line",
+        "alignTicks": false
+    */
+  };
 }());
 
 
