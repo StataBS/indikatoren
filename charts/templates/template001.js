e5d8c291b15ca9cfdae7209ef555367acbb1e828
bl99 <ssslib@bs.ch>
2019-12-11 17:07:45 +0100

    Upload prerequisites for City-Statistics issue-1546

--- charts/templates/template001.js
+++ charts/templates/template001.js
@@ -1,15 +1,55 @@
+/* http://jsfiddle.net/zmktekak/14/ */
+
+var justifyColumns = function (chart) {
+  var categoriesNum = (1 + chart.xAxis[0].max - chart.xAxis[0].min),
+    categoriesWidth = chart.plotSizeX / categoriesNum,
+    each = Highcharts.each,
+    sum, categories = chart.xAxis[0].names,
+    number;
+
+  for (var i = 0; i < categories.length; i++) {
+    sum = 0;
+    each(chart.series, function (p, k) {
+     if (p.visible) {
+        each(p.data, function (ob, j) {
+          if (ob.options.name == categories[i]) {
+            if (ob.options.y != null) {
+              sum++;
+            }
+          }
+        });
+      }
+    });
+    number = 1;
+    each(chart.series, function (p, k) {
+      if (p.visible) {
+        each(p.data, function (ob, j) {
+          if (ob.options.name == categories[i]) {
+            if (ob.options.y != null) {
+              ob.graphic.element.x.baseVal.value = i * categoriesWidth + (categoriesWidth / 2) + ((number - 1) - (sum * 0.5)) * ob.pointWidth;
+              number++;
+            }
+
+          }
+        });
+      }
+    });
+  }
+};
+
+  
 (function(){
     return {
-    "chart": {
-        "borderColor": "#fbfbfb",
-        "backgroundColor": "#fbfbfb",
-        "width": 485,
-        "height": 415,        
+    chart: {
+        borderColor: "#fbfbfb",
+        backgroundColor: "#fbfbfb",
+        width: 485,
+        height: 415,        
         spacing: [10, 10, 30, 10],
-        "style": {
-            "fontFamily": "Arial"
+        style: {
+            fontFamily: "Arial"
         },
-        "zoomType": "xy",
+        zoomType: "xy",
         events: {
           load: function(){
             this.credits.element.onclick = function() {};
@@ -27,91 +67,93 @@
           }
         }        
     },    
-    "plotOptions": {
-        "series": {
+    plotOptions: {
+        series: {
             borderWidth: 0,
-            "dataLabels": {
-                "style": {
-                "fontSize": "10px"
+            dataLabels: {
+                style: {
+                fontSize: "10px"
                 }
             }
         }
     },
-    "title": {
-        "style": {
-        "fontSize": "14px",
-        "fontWeight": "bold",
-        "color": "#000000"
+    title: {
+        style: {
+        fontSize: "14px",
+        fontWeight: "bold",
+        color: "#000000"
         },
-        "align": "left"
+        align: "left"
     },
-    "subtitle": {
-        "style": {
-        "fontSize": "12px",
-        "fontWeight": "normal",
-        "fontFamily": "Arial",
-        "color": "#000000"
+    subtitle: {
+        style: {
+        fontSize: "12px",
+        fontWeight: "normal",
+        fontFamily: "Arial",
+        color: "#000000"
         },
-        "text": "",
-        "align": "left"
+        text: "",
+        align: "left"
     },
-    "xAxis": {
+    xAxis: {
         lineColor: '#B9CFD7', 
         lineWidth: 0.5,
-        "uniqueNames": true,
-        "title": {
-            "style": {
-                "color": "#000000"
+        uniqueNames: true,
+        title: {
+            style: {
+                color: "#000000"
             }
         },
-        "labels": {
-            "style": {
-                "color": "#000000"
+        labels: {
+            style: {
+                color: "#000000"
             }
         },
-        "tickLength": 0
+        tickLength: 0
     }, 
-    "yAxis": {
+    yAxis: {
         gridLineColor: '#B9CFD7', 
         gridLineWidth: 0.5,
         lineColor: '#B9CFD7', 
-        "title": {
-            "style": {
-                "color": "#000000",
-                "fontSize": null
+        title: {
+            style: {
+                color: "#000000",
+                fontSize: null
             },
-            "text": null
+            text: null
         },
-        "labels": {
-            "style": {
-            "color": "#000000"
+        labels: {
+            style: {
+            color: "#000000"
             }
         }
     },    
-	"navigation": {
-		"menuItemStyle": {
-			"fontFamily": Highcharts.SVGRenderer.prototype.getStyle().fontFamily,
-			"padding": '2px 10px'
+	navigation: {
+		menuItemStyle: {
+			fontFamily: Highcharts.SVGRenderer.prototype.getStyle().fontFamily,
+			padding: '2px 10px'
 		}
 	},
-    "credits": {
-        "enabled": true,
-        "style": {
-            "color": "#000000",
-            "fontSize": "10px",
-            "cursor": "default"
+    credits: {
+        enabled: true,
+        style: {
+            color: "#000000",
+            fontSize: "10px",
+            cursor: "default"
         },
-        "position": {
-            "align": "left",
-            "verticalAlign": "bottom",
-            "x": 10
+        position: {
+            align: "left",
+            verticalAlign: "bottom",
+            x: 10
         }
     },
-    "legend": {
-        "symbolRadius": 0, 
-        "itemStyle": {
-            "fontWeight": "normal"
+    legend: {
+        symbolRadius: 0, 
+        itemStyle: {
+            fontWeight: "normal"
         }    
-    }
+    },
+    
 	};
 }());
+
