c85bc2d575e0a4327d6ad333403449f0c3f35f8b
dmathys <daniel.mathys@bs.ch>
2020-01-23 12:09:46 +0100

    Remove obsolete scripts, fix build script

--- build/_checkoutChartFromMaster.sh
+++
@@ -1,3 +0,0 @@
-git checkout master -- charts/templates/$1.js
-git checkout master -- data/$1.tsv
-git checkout master -- metadata/single/$1.json
--- build/_copyTemplateAndTsv.sh
+++
@@ -1,2 +0,0 @@
-cp charts/templates/$1.js charts/templates/$2.js 
-cp data/$1.tsv data/$2.tsv
