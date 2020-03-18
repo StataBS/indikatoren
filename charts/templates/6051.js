d66ea70f884fc76efe29e734ad53ff3acaecfb84
dmathys <daniel.mathys@bs.ch>
2020-03-18 08:49:07 +0100

    separate download of .js and .tsv from ftp; Live schalten

--- charts/templates/6051.js
+++ charts/templates/6051.js
@@ -1,43 +1,67 @@
-(function(){
+(function () {
   return {
-    "yAxis": [
-      {
-        "labels": {
-          "format": "{value:,.0f}",
-          "style": {
-            "color": "#000000"
-          }
+    "data": {
+      "seriesMapping": [
+        {
+          x: 0, y: 1
         },
-        "min": 0,
-        "max": undefined,
-        "title": {
-          enabled: false
+        {
+          x: 0, y: 2
         }
-      },
-      {
-        "opposite": true,
-        "title": {
-          enabled: false
-        },
-        "min": 0,
-        "max": 240000,
-        "gridLineWidth": 0,
-        "labels": {
-          "style": {
-            "color": "#000000"
+      ]
+    },
+    yAxis: {
+      min: null,
+      plotLines: [{
+        color: '#B9CFD7',
+        value: 100,
+        width: 2
+      }]
+    },
+    /*    "yAxis": [
+          {
+            "labels": {
+              "format": "{value:,.0f}",
+              "style": {
+                "color": "#000000"
+              }
+            },
+            "min": 0,
+            "max": undefined,
+            "title": {
+              enabled: false
+            }
+          },
+          {
+            "opposite": true,
+            "title": {
+              enabled: false
+            },
+            "min": 0,
+            "max": 240000,
+            "gridLineWidth": 0,
+            "labels": {
+              "style": {
+                "color": "#000000"
+              }
+            }
           }
-        }
-      }
-    ],
+        ],
+    */
     "xAxis": {
-  	  "tickInterval": 2
+      "tickInterval": 1,
+      /*plotLines: [{
+        color: '#B9CFD7',
+        value: 2000,
+        width: 1
+      }]*/
     },
     "series": [
       {
         "marker": {
           enabled: false,
           "symbol": "circle"
-        },     
+        },
         "color": "#008AC3",
         "yAxis": 0,
         "tooltip": {
@@ -48,13 +72,36 @@
         "marker": {
           enabled: false,
           "symbol": "circle"
-        },     
+        },
         "color": "#68AB2B",
-        "yAxis": 1,
+        "yAxis": 0,
         "tooltip": {
           "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
         }
       }
+      /*      {
+              "marker": {
+                enabled: false,
+                "symbol": "circle"
+              },     
+              "color": "#008AC3",
+              "yAxis": 0,
+              "tooltip": {
+                "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
+              },
+            },
+            {
+              "marker": {
+                enabled: false,
+                "symbol": "circle"
+              },     
+              "color": "#68AB2B",
+              "yAxis": 1,
+              "tooltip": {
+                "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
+              }
+            }
+      */
     ],
     "legend": {
       "enabled": true,
@@ -65,10 +112,7 @@
       "verticalAlign": "top",
       "itemMarginBottom": 5,
       "align": "left",
-      "itemStyle": {
-        "fontWeight": "normal"
-      }
-    },
+    }
   }
 }());
 
---
+++ charts/templates/8453.js
@@ -0,0 +1,53 @@
+(function () {
+	return {
+		"chart": {
+			marginLeft: 100,
+			//marginBottom: 100,
+			"inverted": true,
+		},
+		plotOptions: {
+			column: {
+				groupPadding: 0,
+			}
+		},
+		"xAxis": {
+			"type": "category",
+			reversed: false,
+			"labels": {
+				align: "left",
+				x: -80,
+				"formatter": function () {
+					//add sum of observations of visible series to the axis label
+					var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
+						return val.visible;
+					});
+					var indexOfCurrentValue = this.axis.names.indexOf(this.value);
+					var sum = allVisibleSeries.reduce(function (accumulator, series, index, arr) {
+						return accumulator + series.yData[indexOfCurrentValue];
+					}, 0);
+					//use N if all series are visible, otherwise use n
+					var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n=';
+					//check for value that contains only spaces
+					return (this.value.replace(/\s/g, "") == "") ? this.value : this.value + ' (' + nString + sum + ')';
+				}
+			}
+		},
+		yAxis: {
+			tickInterval: 20,
+			max: 100,
+		},
+		"series": [
+			{ "color": "#007A2F", index: 5, legendIndex: 1 }, /*dunkelgrün*/
+			{ "color": "#73B97C", index: 4, legendIndex: 2 }, /*hellgrün*/
+			{ "color": "#FABD24", index: 3, legendIndex: 3 }, /*hellrot*/
+			{ "color": "#CD9C00", index: 2, legendIndex: 4 }, /*dunkelrot*/
+			{ "color": "#C8C8C8", index: 1, legendIndex: 5 },
+			{ "color": "#6F6F6F", index: 0, legendIndex: 6 },
+		],
+		"legend": {
+			"layout": "horizontal",
+			"verticalAlign": "top",
+			"align": "left",
+		},
+	}
+}());
