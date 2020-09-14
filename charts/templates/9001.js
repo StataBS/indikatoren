1afb8d35186ce6fc0a3f0821796212e80959d005
dmathys <daniel.mathys@bs.ch>
2020-09-14 08:43:14 +0200

    Live schalten

---
+++ charts/templates/9001.js
@@ -0,0 +1,39 @@
+(function () {
+  return {
+    chart: {
+      type: "column",
+      animation: false,
+      inverted: false,
+    },
+    plotOptions: {
+      series: {
+        pointPadding: 0,
+        borderWidth: 0,
+        pointWidth: 50
+      }
+    },
+    yAxis: {
+      //tickAmount: 6
+      min: 0,
+      labels: {
+        format: "{value:,.0f}%"
+      }
+    },
+    series: [
+      { color: "#FF8028" }
+    ],
+    legend: {
+      enabled: false,
+      layout: "horizontal",
+      verticalAlign: "top",
+      align: "left",
+      itemWidth: 300,
+      itemStyle: {
+        fontWeight: "normal"
+      }
+    },
+    tooltip: {
+      pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}%</b><br/>',
+    }
+  }
+}());
