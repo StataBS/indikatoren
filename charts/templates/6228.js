2387f22b28d895a9b7c2e9c7536a78e6946b2889
dmathys <daniel.mathys@bs.ch>
2020-11-11 08:15:39 +0100

    Live schalten

--- charts/templates/6228.js
+++ charts/templates/6228.js
@@ -1,15 +1,20 @@
 (function () {
     return {
-        "yAxis": {
+        chart: {
+            inverted: true,
+        },
+        plotOptions: {
+            column: {
+                groupPadding: 0,
+            }
+        },
+        yAxis: {
             tickInterval: 25,
         },
-        "xAxis": {
-            "type": "category",
-            reversed: false,
-            "labels": {
-                align: "left",
-                x: -155,
-                "formatter": function () {
+        xAxis: {
+            type: "category",
+            labels: {
+                formatter: function () {
                     //add sum of observations of visible series to the axis label
                     var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
                         return val.visible;
@@ -19,15 +24,19 @@
                         return accumulator + series.yData[indexOfCurrentValue];
                     }, 0);
                     //use N if all series are visible, otherwise use n
-                    var nString = /*(this.chart.series.length == allVisibleSeries.length) ? 'N=' :*/ 'n=';
+                    var nString = /*(this.chart.series.length == allVisibleSeries.length) ? 'N=' : */'n=';
+
+                    //delete everything before ":", including ":"
+                    this.value = this.value.replace(/[^:]*:/, "");
+
                     //check for value that contains only spaces
-                    return (this.value.replace(/\s/g, "") == "") ? this.value : this.value + ' (' + nString + sum + ')';
+                    if (sum != 0) return (this.value.replace(/\s/g, "") == "") ? this.value : this.value + ' (' + nString + sum + ')';
+                    //else, if sum = 0, then it is assumed to be an intermediate title. return it bold
+                    return "<b>" + this.value + "</b>";
                 }
             }
         },
         tooltip: {
-            //"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:,.1f}%)<br/>',
-            //"footerFormat": '<br/>Notenschnitt: <b>{point[2].y:,.1f}</b><br/>',
             shared: true,
             useHTML: true,
             followPointer: true,
@@ -43,27 +52,17 @@
                 return s;
             },
         },
-        plotOptions: {
-            column: {
-                groupPadding: 0,
-
-            }
-        },
-        "series": [
-            { "color": "#007a2f", "index": 9, "legendIndex": 0 }, /*grün dunkel2*/
-            { "color": "#44ab2b", "index": 8, "legendIndex": 1 }, /*grün mittel*/
-            { "color": "#73ba7c", "index": 7, "legendIndex": 2 }, /*grün dunkel1*/
-            { "color": "#d7e9d2", "index": 6, "legendIndex": 3 }, /*grün hell*/
-            { "color": "#b9cfd7", "index": 5, "legendIndex": 4 }, /*blau hell */
-            { "color": "#71a3b5", "index": 4, "legendIndex": 5 }, /*blau dunkel */
-            { "color": "#ffbb58", "index": 3, "legendIndex": 6 }, /*orange hell */
-            { "color": "#ff8028", "index": 2, "legendIndex": 7 }, /*orange dunkel */
-            { "color": "#dc440e", "index": 1, "legendIndex": 8 }, /*rot hell */
-            { "color": "#b00000", "index": 0, "legendIndex": 9 } /*rot dunkel */
-        ],
-        "chart": {
-            marginLeft: 165,
-            "inverted": true,
-        },
+        series: [
+            { color: "#007a2f", index: 9, legendIndex: 0 }, /*grün dunkel2*/
+            { color: "#44ab2b", index: 8, legendIndex: 1 }, /*grün mittel*/
+            { color: "#73ba7c", index: 7, legendIndex: 2 }, /*grün dunkel1*/
+            { color: "#d7e9d2", index: 6, legendIndex: 3 }, /*grün hell*/
+            { color: "#b9cfd7", index: 5, legendIndex: 4 }, /*blau hell */
+            { color: "#71a3b5", index: 4, legendIndex: 5 }, /*blau dunkel */
+            { color: "#ffbb58", index: 3, legendIndex: 6 }, /*orange hell */
+            { color: "#ff8028", index: 2, legendIndex: 7 }, /*orange dunkel */
+            { color: "#dc440e", index: 1, legendIndex: 8 }, /*rot hell */
+            { color: "#b00000", index: 0, legendIndex: 9 }  /*rot dunkel */
+          ],
     }
 }());
