c6a0b08d8cd35a9ec887dc38bcd1d07e5b79bbaf
dmathys <daniel.mathys@bs.ch>
2020-11-12 09:08:50 +0100

    Live schalten

--- charts/templates/6516.js
+++ charts/templates/6516.js
@@ -1,43 +1,50 @@
-(function(){
-    return {
- 	"xAxis": {
-        "type": "category",
-        "labels": {
-           align: "left",
-            x: -180,
-            "formatter": function() {
-                //add sum of observations of visible series to the axis label
-                var allVisibleSeries = this.chart.series.filter(function(val, i, arr){
-                    return val.visible;
-                });
-                var indexOfCurrentValue = this.axis.names.indexOf(this.value);
-                var sum = allVisibleSeries.reduce(function(accumulator, series, index, arr){
-                    return accumulator + series.yData[indexOfCurrentValue];
-                }, 0);
-                //use N if all series are visible, otherwise use n
-                var nString = (this.chart.series.length == allVisibleSeries.length && this.value.indexOf('Total') != -1) ? 'N=' : 'n='; 
-                //check for value that contains only spaces
-            	return (this.value.replace(/\s/g,"") == "") ? this.value : this.value + ' (' + nString + sum + ')';
-            }
-        } 
-  },  
-  yAxis:{
-  	tickInterval: 20,
-  	max: 100,
-  	labels:{
-  		"type": "category",
-  		rotation: 0,
-  		style:{
-  			 textOverflow: 'none' // prevents ellipsis
-  		}
-  	}
-  },
-  plotOptions: {
-  	column: {
-  		groupPadding: 0,
+(function () {
+  return {
+      chart: {
+          inverted: true,
+      },
+      plotOptions: {
+          column: {
+              groupPadding: 0,
+          }
+      },
+      yAxis: {
+          tickInterval: 20,
+      },
+      xAxis: {
+          type: "category",
+          labels: {
+              formatter: function () {
+                  //add sum of observations of visible series to the axis label
+                  var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
+                      return val.visible;
+                  });
+                  var indexOfCurrentValue = this.axis.names.indexOf(this.value);
+                  var sum = allVisibleSeries.reduce(function (accumulator, series, index, arr) {
+                      return accumulator + series.yData[indexOfCurrentValue];
+                  }, 0);
+                  //use N if all series are visible, otherwise use n
+                  var nString =  'n=';
+                    if (this.value.match(/Total/)) nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n=';
 
-  	}
-  },
+                  //delete everything before ":", including ":"
+                  this.value = this.value.replace(/[^:]*:/, "");
+
+                  //check for value that contains only spaces
+                  if (sum != 0) return (this.value.replace(/\s/g, "") == "") ? this.value : this.value + ' (' + nString + sum + ')';
+                  //else, if sum = 0, then it is assumed to be an intermediate title. return it bold
+                  return "<b>" + this.value + "</b>";
+              }
+          }
+      },
+      "legend": {
+        "enabled": true,
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "itemMarginBottom": 5,
+        "align": "left"
+      },
+
   "series": [
     { "color": "#007a2f", index: 6, legendIndex: 0}, // dunkelgrün
     { "color": "#68ab2b", index: 5, legendIndex: 1}, // grün
@@ -47,22 +54,5 @@
     { "color": "#6F6F6F", index: 0, legendIndex: 6}, // dunkelgrau
 
   ],
-"chart": {     
-	 marginLeft: 195, 
-	 //marginBottom: 100,
-     "inverted": true,
-  },
-  "legend": { 
-     //y: 45,
-     x: -5,
-    "enabled": true,
-    "layout": "horizontal",
-   "itemWidth": 150,
-    "verticalAlign": "top",
-    "align": "left",
-    "itemStyle": {
-    "fontWeight": "normal"
-    }
-  },
 }
 }());
