0117270565f1cc86e0d6dd83e2e0057c835a3882
nim-stata <nim@statabs.ch>
2022-03-03 08:37:57 +0100

    Liveschalten

--- metadata/sets/Monatsindikatoren.js
+++ metadata/sets/Monatsindikatoren.js
@@ -109,7 +109,7 @@ var indikatorensetData = [
 		"darstellungsart": "Diagramm",
 		"printformat": "",
 		"datenInChartIntegriert": false,
-		"aktualisierungsdatum": "2022-02-11T08:34:14",
+		"aktualisierungsdatum": "2022-03-03T08:34:59",
 		"id": 7512
 	},
 	{
@@ -147,7 +147,7 @@ var indikatorensetData = [
 		"darstellungsart": "Diagramm",
 		"printformat": "",
 		"datenInChartIntegriert": false,
-		"aktualisierungsdatum": "2022-02-11T08:34:15",
+		"aktualisierungsdatum": "2022-03-03T08:34:59",
 		"id": 7513
 	},
 	{
