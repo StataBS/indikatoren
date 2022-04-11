d46bfdd20300f64abed9cde69fa92bdea4aa8839
dmathys <daniel.mathys@bs.ch>
2022-04-11 08:30:42 +0200

    Live schalten

--- charts/templates/5197.js
+++ charts/templates/5197.js
@@ -1,52 +1,42 @@
-(function(){
-    return {
-   plotOptions: {
-        series: {
-            pointPadding: 0,
-            borderWidth: 0,
-        }
-   },
-
-  "data":{
-  	"switchRowsAndColumns": false
-  },
-  "xAxis": {
-    "type": "category"
-  },
-  "yAxis": {
-  	//"min": 0,
-  	//"max": 0.5,
-    "labels": {
-      "format": "{value}%"
-    }    
-  },
-  "series": [
-  {"color": "#b00000"},
-  {"color": "#4f81bd"}, 
-  {"color": "#9A86A6"}, 
-  //{"color": "#9bbb59"},  
-  ],
-  "legend": {
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-    //"x": 25,
-	  //"y": 25,  
-	  "itemWidth": 300,
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
-  },
-  tooltip: {
-    pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>',
-    shared: false
-  },  
-  "chart": {      
-    "type": "column",
-    "inverted": false
+(function () {
+  return {
+    "chart": {
+      "type": "column",
+    },
+    plotOptions: {
+      series: {
+        pointPadding: 0,
+        borderWidth: 0,
+      }
+    },
+    "xAxis": {
+      tickPositioner: function () {
+        var maxlabels = 7,
+          ext = this.getExtremes(),
+          i = Math.round(ext.max),
+          interval = Math.round((i - ext.min)/maxlabels),
+          pos = [i];
+        while (i >= ext.min) pos.unshift(i = i - interval);
+        return pos;
+      }
+    },
+    "yAxis": {
+      "labels": {
+        "format": "{value}%"
+      }
+    },
+    "series": [
+      { "color": "#b00000" },
+      { "color": "#4f81bd" },
+    ],
+    "legend": {
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+    },
+    tooltip: {
+      pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>',
+    },
   }
-}
 }());
 
- 
