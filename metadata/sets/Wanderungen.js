9899d484717805af230f60aebbdb147c67270d41
dmathys <daniel.mathys@bs.ch>
2022-06-13 08:37:02 +0200

    Live schalten

--- metadata/sets/Wanderungen.js
+++ metadata/sets/Wanderungen.js
@@ -1,45 +1,44 @@
 var indikatorensetData = [
 	{
+		"template": "template001",
+		"data-id": 6011,
+		"chart-id": 6011,
+		"filter": "",
 		"parentId": null,
 		"children": [
-			"5155",
-			"6760",
-			"8887"
+			5155,
+			6760,
+			8887,
+			9599
 		],
-		"template": "template001",
 		"title": "Zu- und Weggezogene nach Zuzugsort und Wegzugsort",
 		"subtitle": "Basel-Stadt",
-		"kennzahlenset": "Wanderungen",
-		"kuerzel": "I.01.5.6011",
-		"kuerzelKunde": "2-1",
-		"description": "Zu- und Weggezogene nach Zuzugsort und Wegzugsort",
-		"thema": "01 Bevölkerung",
-		"unterthema": "Wanderungen",
+		"lesehilfe": "2021 ergab sich durch Zu- und Wegzüger im Kanton Basel-Stadt ein Minus von 1 000 Personen. Damit ist der Wanderungssaldo der betrachteten zehn Jahre das erste Mal negativ. Die meisten Wegzüger des Jahres 2021 (rund 5 200 Personen) gingen in die Nordwestschweiz. Die meisten Zuzüger kamen aus dem Ausland nach Basel-Stadt, zuletzt fast 6 800 Personen.",
+		"erlaeuterungen": "Nordwestschweiz: Kanton Basel-Landschaft, Bezirke Dorneck und Thierstein des Kantons Solothurn sowie Bezirke Laufenburg und Rheinfelden einschliesslich der Gemeinden Bözen, Effingen und Elfingen des Kantons Aargau.",
+		"quellenangabe": [
+			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
+		],
+		"externalLinks": [],
+		"renderLink": [],
 		"raeumlicheGliederung": [
 			"Kanton"
 		],
-		"aktualisierungsdatum": "2021-05-20T08:05:26.2626",
+		"description": "Zu- und Weggezogene nach Zuzugsort und Wegzugsort",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Wanderungen",
+		"kuerzel": "I.01.5.06011",
+		"kennzahlenset": "Wanderungen",
+		"kuerzelKunde": "2-1",
 		"stufe1": "Kernindikatoren zu den Wanderungsbewegungen",
 		"stufe2": "",
 		"stufe3": "",
 		"stufe4": "",
 		"stufe5": "",
-		"lesehilfe": "2020 zogen 13 480 Personen neu in den Kanton Basel-Stadt. Dies ist der niedrigste Wert der letzten zehn Jahre. 12 524 Personen verliessen den Kanton im Jahr 2020. Letztmals weniger Weggezogene wurden 2012 - nämlich 12 441 - registriert. Der Wanderungssaldo fiel zwischen 2011 und 2020 stets positiv aus und ging jeweils einzig auf die Zuwanderung aus dem Ausland zurück. Im Vergleich zum Jahr 2019 hat die Zahl der Wanderungsbewegungen zwischen Basel-Stadt und der Nordwestschweiz sowie der übrigen Schweiz zugenommen, während die Zahl der Wanderungsbewegungen zwischen Basel-Stadt und dem Ausland zurückgegangen ist.",
-		"erlaeuterungen": "Nordwestschweiz: Kanton Basel-Landschaft, Bezirke Dorneck und Thierstein des Kantons Solothurn sowie Bezirke Laufenburg und Rheinfelden einschliesslich der Gemeinden Bözen, Effingen und Elfingen des Kantons Aargau.",
+		"orderKey": "01_00_00_00_00_001",
 		"darstellungsart": "Diagramm",
 		"printformat": "",
-		"orderKey": "01_00_00_00_00_001",
-		"renderLink": [],
-		"quellenangabe": [
-			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
-		],
 		"datenInChartIntegriert": false,
-		"externalLinks": [
-			""
-		],
-		"data-id": 6011,
-		"chart-id": 6011,
-		"filter": "",
+		"aktualisierungsdatum": "2022-06-13T08:03:36",
 		"id": 6011
 	},
 	{
@@ -84,85 +83,81 @@ var indikatorensetData = [
 		"id": 6012
 	},
 	{
-		"parentId": null,
-		"children": [
-			"6762"
-		],
 		"template": "pie001",
-		"title": "Aus dem Ausland Zugezogene nach Zuzugsland",
-		"subtitle": "Basel-Stadt, 2020",
-		"kennzahlenset": "Wanderungen",
-		"kuerzel": "",
-		"kuerzelKunde": "2-3",
-		"description": "Aus dem Ausland Zugezogene nach Zuzugsland",
-		"thema": "01 Bevölkerung",
-		"unterthema": "Wanderungen",
-		"raeumlicheGliederung": [
-			"Kanton"
-		],
-		"aktualisierungsdatum": "2021-05-26T08:36:42.4242",
-		"stufe1": "Kernindikatoren zu den Wanderungsbewegungen",
-		"stufe2": "",
-		"stufe3": "",
-		"stufe4": "",
-		"stufe5": "",
-		"lesehilfe": "Die Grafik zeigt die fünf wichtigsten ausländischen Zuzugsländer des Jahres 2020. Die grösste Gruppe der aus dem Ausland Zugewanderten kommt aus Deutschland (1 949 Personen), danach folgen Personen aus dem Vereinigten Königreich (469) und Frankreich (466). Aus Italien kommen 455 Personen, aus den Vereinigten Staaten 405. Die übrigen 3 312 Zuzügerinnen und Zuzüger verteilen sich auf 130 verschiedene Länder.",
-		"erlaeuterungen": "",
-		"darstellungsart": "Diagramm",
-		"printformat": "",
-		"orderKey": "01_00_00_00_00_003",
-		"renderLink": [],
-		"quellenangabe": [
-			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
-		],
-		"datenInChartIntegriert": false,
-		"externalLinks": [
-			""
-		],
 		"data-id": 6013,
 		"chart-id": 6013,
 		"filter": "",
-		"id": 6013
-	},
-	{
 		"parentId": null,
 		"children": [
-			"6765"
+			6762
 		],
-		"template": "template001",
-		"title": "Zugezogene nach Zuzugsland, bei denen die Staatsangehörigkeit dem Zuzugsland entspricht",
-		"subtitle": "Basel-Stadt",
-		"kennzahlenset": "Wanderungen",
-		"kuerzel": "",
-		"kuerzelKunde": "2-6",
-		"description": "Zugezogene nach Zuzugsland, bei denen die Staatsangehörigkeit dem Zuzugsland entspricht",
-		"thema": "01 Bevölkerung",
-		"unterthema": "Wanderungen",
+		"title": "Aus dem Ausland Zugezogene nach Zuzugsland",
+		"subtitle": "Basel-Stadt, 2021",
+		"lesehilfe": "Die Grafik zeigt die fünf wichtigsten ausländischen Zuzugsländer des Jahres 2021. Die grösste Gruppe der aus dem Ausland Zugewanderten kommt aus Deutschland (1 909 Personen), danach folgen Personen aus Spanien (437), Frankreich (427),  Italien (362) und den Vereinigten Staaten (362). Die übrigen 3 231 Zuzügerinnen und Zuzüger verteilen sich auf über 120 verschiedene Länder.",
+		"erlaeuterungen": "",
+		"quellenangabe": [
+			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
+		],
+		"externalLinks": [],
+		"renderLink": [],
 		"raeumlicheGliederung": [
 			"Kanton"
 		],
-		"aktualisierungsdatum": "2021-05-26T08:36:43.4343",
+		"description": "Aus dem Ausland Zugezogene nach Zuzugsland",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Wanderungen",
+		"kuerzel": "I.01.5.06013",
+		"kennzahlenset": "Wanderungen",
+		"kuerzelKunde": "2-3",
 		"stufe1": "Kernindikatoren zu den Wanderungsbewegungen",
 		"stufe2": "",
 		"stufe3": "",
 		"stufe4": "",
 		"stufe5": "",
-		"lesehilfe": "Bei 59% der Personen, die 2020 aus der Schweiz nach Basel-Stadt zuzogen, handelte es sich um Schweizer Staatsangehörige. Dieser Wert hat sich gegenüber 2011 kaum verändert. Der Anteil der deutschen Staatsangehörigen an allen aus Deutschland Zugewanderten betrug 73%, derjenige der italienischen Staatsangehörigen an allen aus Italien Zugewanderten 88%.",
-		"erlaeuterungen": "",
+		"orderKey": "01_00_00_00_00_003",
 		"darstellungsart": "Diagramm",
 		"printformat": "",
-		"orderKey": "01_00_00_00_00_006",
-		"renderLink": [],
-		"quellenangabe": [
-			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
-		],
 		"datenInChartIntegriert": false,
-		"externalLinks": [
-			""
-		],
+		"aktualisierungsdatum": "2022-06-13T08:03:36",
+		"id": 6013
+	},
+	{
+		"template": "template001",
 		"data-id": 6016,
 		"chart-id": 6016,
 		"filter": "",
+		"parentId": null,
+		"children": [
+			6765
+		],
+		"title": "Zugezogene nach Zuzugsland, bei denen die Staatsangehörigkeit dem Zuzugsland entspricht",
+		"subtitle": "Basel-Stadt",
+		"lesehilfe": "60% der Personen, die 2021 aus der Schweiz nach Basel-Stadt ziehen, sind Schweizer Staatsangehörige (2012: 58,5%). 75% der aus Deutschland zuwandernden Personen sind deutsche und 83% der aus Spanien zuziehenden Personen sind spanische Staatsangehörige.",
+		"erlaeuterungen": "",
+		"quellenangabe": [
+			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
+		],
+		"externalLinks": [],
+		"renderLink": [],
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"description": "Zugezogene nach Zuzugsland, bei denen die Staatsangehörigkeit dem Zuzugsland entspricht",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Wanderungen",
+		"kuerzel": "I.01.5.06016",
+		"kennzahlenset": "Wanderungen",
+		"kuerzelKunde": "2-6",
+		"stufe1": "Kernindikatoren zu den Wanderungsbewegungen",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"orderKey": "01_00_00_00_00_006",
+		"darstellungsart": "Diagramm",
+		"printformat": "",
+		"datenInChartIntegriert": false,
+		"aktualisierungsdatum": "2022-06-13T08:03:37",
 		"id": 6016
 	},
 	{
@@ -207,44 +202,42 @@ var indikatorensetData = [
 		"id": 6017
 	},
 	{
+		"template": "populationPyramid001",
+		"data-id": 6018,
+		"chart-id": 6018,
+		"filter": "",
 		"parentId": null,
 		"children": [
-			"6767"
+			6767
 		],
-		"template": "populationPyramid001",
 		"title": "Zuzug aus der Schweiz, Wegzug in die Schweiz und Wanderungssaldo nach Alter",
-		"subtitle": "Basel-Stadt, 2011 bis 2020 zusammengefasst",
-		"kennzahlenset": "Wanderungen",
-		"kuerzel": "",
-		"kuerzelKunde": "2-8",
-		"description": "Zuzug aus der Schweiz, Wegzug in die Schweiz und Wanderungssaldo nach Alter",
-		"thema": "01 Bevölkerung",
-		"unterthema": "Wanderungen",
+		"subtitle": "Basel-Stadt, 2012 bis 2021 zusammengefasst",
+		"lesehilfe": "Zwischen 2012 und 2021 war die Wanderungsbilanz zwischen den aus der Schweiz Zugezogenen und den in die Schweiz Weggezogenen bei zwei jüngeren Altersgruppen (15- bis 19- und 20- bis 24-Jährige) sowie den 75- bis 79- und den über 79-Jährigen positiv.",
+		"erlaeuterungen": "",
+		"quellenangabe": [
+			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
+		],
+		"externalLinks": [],
+		"renderLink": [],
 		"raeumlicheGliederung": [
 			"Kanton"
 		],
-		"aktualisierungsdatum": "2021-05-26T08:36:44.4444",
+		"description": "Zuzug aus der Schweiz, Wegzug in die Schweiz und Wanderungssaldo nach Alter",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Wanderungen",
+		"kuerzel": "I.01.5.06018",
+		"kennzahlenset": "Wanderungen",
+		"kuerzelKunde": "2-8",
 		"stufe1": "Kernindikatoren zu den Wanderungsbewegungen",
 		"stufe2": "",
 		"stufe3": "",
 		"stufe4": "",
 		"stufe5": "",
-		"lesehilfe": "Zwischen 2011 und 2020 war die Wanderungsbilanz zwischen den aus der Schweiz Zugezogenen und den in die Schweiz Weggezogenen nur bei den 15- bis 19-Jährigen, den 20- bis 24-Jährigen sowie bei den 75- bis 79-Jährigen und den über 79-Jährigen (knapp) positiv.",
-		"erlaeuterungen": "",
+		"orderKey": "01_00_00_00_00_008",
 		"darstellungsart": "Diagramm",
 		"printformat": "",
-		"orderKey": "01_00_00_00_00_008",
-		"renderLink": [],
-		"quellenangabe": [
-			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
-		],
 		"datenInChartIntegriert": false,
-		"externalLinks": [
-			""
-		],
-		"data-id": 6018,
-		"chart-id": 6018,
-		"filter": "",
+		"aktualisierungsdatum": "2022-06-13T08:03:37",
 		"id": 6018
 	},
 	{
@@ -373,44 +366,42 @@ var indikatorensetData = [
 		"id": 6022
 	},
 	{
+		"template": "template001",
+		"data-id": 6023,
+		"chart-id": 6023,
+		"filter": "",
 		"parentId": null,
 		"children": [
-			"6772"
+			6772
 		],
-		"template": "template001",
 		"title": "Zugezogene, Weggezogene und Wanderungssaldo",
-		"subtitle": "nach Zuzugs- und Wegzugsland, Basel-Stadt, 2020",
-		"kennzahlenset": "Wanderungen",
-		"kuerzel": "I.01.5.6023",
-		"kuerzelKunde": "2-4",
-		"description": "Zugezogene, Weggezogene und Wanderungssaldo",
-		"thema": "01 Bevölkerung",
-		"unterthema": "Wanderungen",
+		"subtitle": "nach Zuzugs- und Wegzugsland, Basel-Stadt, 2021",
+		"lesehilfe": "Betrachtet man die fünf wichtigsten ausländischen Zuzugsländer, verbuchte Basel-Stadt 2021 den grössten Zuzugsüberschuss gegenüber Deutschland (+666), danach folgt Spanien (+257).",
+		"erlaeuterungen": "",
+		"quellenangabe": [
+			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
+		],
+		"externalLinks": [],
+		"renderLink": [],
 		"raeumlicheGliederung": [
 			"Kanton"
 		],
-		"aktualisierungsdatum": "2021-06-02T09:03:13.1313",
+		"description": "Zugezogene, Weggezogene und Wanderungssaldo",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Wanderungen",
+		"kuerzel": "I.01.5.6023",
+		"kennzahlenset": "Wanderungen",
+		"kuerzelKunde": "2-4",
 		"stufe1": "Kernindikatoren zu den Wanderungsbewegungen",
 		"stufe2": "",
 		"stufe3": "",
 		"stufe4": "",
 		"stufe5": "",
-		"lesehilfe": "Betrachtet man die fünf wichtigsten ausländischen Zuzugsländer, verbuchte Basel-Stadt 2020 den grössten Zuzugsüberschuss gegenüber Deutschland (+746). Den zweiten und dritten Rang belegten das Vereinigte Königreich (+262) und Italien (+261). Danach folgten die Vereinigten Staaten (+169) und Frankreich (+98).",
-		"erlaeuterungen": "",
+		"orderKey": "01_00_00_00_00_004",
 		"darstellungsart": "Diagramm",
 		"printformat": "",
-		"orderKey": "01_00_00_00_00_004",
-		"renderLink": [],
-		"quellenangabe": [
-			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
-		],
 		"datenInChartIntegriert": false,
-		"externalLinks": [
-			""
-		],
-		"data-id": 6023,
-		"chart-id": 6023,
-		"filter": "",
+		"aktualisierungsdatum": "2022-06-13T08:03:36",
 		"id": 6023
 	},
 	{
