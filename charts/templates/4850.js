f391970d6042d4792bad8d30ecfe338ceaf0edc9
Marcel Colomb <marcel.colomb@bs.ch>
2023-03-08 08:22:45 +0100

    Live schalten

--- charts/templates/4850.js
+++ charts/templates/4850.js
@@ -3,29 +3,49 @@
         "series": [
             {
             "color": "#B475AB",
-            "index": 4 
+            "index": 6,
+            "legendIndex": 0
             },
             {
             "color": "#71A3B5",
-            "index": 3 
+            "index": 5, 
+            "legendIndex": 1
             },
             {
             "color": "#FFBB58",
-            "index": 2 
+            "index": 4,
+            "legendIndex": 2 
             },
             {
+            "color": "#ff8028",
+            "index": 3,
+            "legendIndex": 3 
+                },
+                {
+            "color": "#73ba7c",
+            "index": 2,
+            "legendIndex": 4
+                },        
+            {
             "color": "#C8C8C8",
-            "index": 1 
+            "index": 1,
+            "legendIndex": 5 
             },
             {
             "color": "#8A8A8A",
-            "index": 0 
-            }        
+            "index": 0,
+            "legendIndex": 6
+              }        
         ],
         "xAxis": {
             "type": "category"
         },
-        "legend": {
+        "legend":  
+        {"enabled": true,
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "itemMarginBottom": 5, // space between legend boxes
+        "align": "left",
             "itemStyle": {
                 "width": 150
             }
