663a3b980a0a253caf81ed866b5a24bc3a75c082
Marcel Colomb <marcel.colomb@bs.ch>
2023-08-28 08:48:15 +0200

    Live Schalten

--- metadata/portal/indikatoren.js
+++ metadata/portal/indikatoren.js
@@ -14416,7 +14416,7 @@ var indikatoren = [
 		"printformat": "",
 		"visibleInPortal": true,
 		"datenInChartIntegriert": false,
-		"aktualisierungsdatum": "2023-08-25T10:08:57",
+		"aktualisierungsdatum": "2023-08-28T08:43:54",
 		"id": 6236
 	},
 	{
