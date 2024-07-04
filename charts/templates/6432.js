a02daf20e7ae43f2ec1816a84056a5703c5c453c
nim-stata <nim@statabs.ch>
2024-07-04 09:26:21 +0200

    Liveschalten

--- charts/templates/6432.js
+++ charts/templates/6432.js
@@ -13,7 +13,7 @@
   "tooltip": {
     "shared": false, 
     headerFormat: '<span style="font-size: 10px">Alter: {point.key}</span><br/>',
-	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}%</b><br/>'
+	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>'
   },
  "series": [
  {"color":  "#b00000"}, /**/
